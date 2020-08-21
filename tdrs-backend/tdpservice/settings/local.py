db81226a3a295b0ebe01e4583144b8e8535e9a52
Carlton Smith <carl@llwebconsulting.com>
2020-08-21 15:05:02 -0400

    Login/Logout functionality
    Added methods to the CustomAuthentication class for handling HttpOnly
    cookie authentication. Added authorization_check endpoint to finalize
    login functionality. Updated settings with CORS whitelist for local.
    
    Setup local env file to reference local api

--- tdrs-backend/tdpservice/settings/local.py
+++ tdrs-backend/tdpservice/settings/local.py
@@ -31,3 +31,8 @@ class Local(Common):
     EMAIL_HOST = 'localhost'
     EMAIL_PORT = 1025
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    # CORS
+    CORS_ORIGIN_WHITELIST = [
+        "http://localhost:3000",
+    ]
