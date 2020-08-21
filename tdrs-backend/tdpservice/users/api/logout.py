8d3c33471ccd7f4c03538900036e433113170389
Adam Caron <adamwcaron@gmail.com>
2020-08-21 10:09:38 -0400

    Change internal response to redirect
    So the API redirects to the UI with an http_only cookie after
    the login.gov flow is complete.
    
    Also, logout redirects back to UI

--- tdrs-backend/tdpservice/users/api/logout.py
+++ tdrs-backend/tdpservice/users/api/logout.py
@@ -1,6 +1,8 @@
 """Login.gov/logout is redirected to this endpoint end a django user session."""
 
+import os
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from rest_framework import status
 from rest_framework.views import APIView
 from django.contrib.auth import logout
@@ -17,6 +19,6 @@ class LogoutUser(APIView):
         except Exception:
             return HttpResponse({
                 "system: User logged out of Login.gov/ Django sessions terminated before local logout"}, status=status.HTTP_200_OK)
-        return HttpResponse({
-            "system: User logged out"
-        }, status=status.HTTP_200_OK)
+        response = HttpResponseRedirect(os.environ['FRONTEND_BASE_URL'])
+        response.delete_cookie('id_token')
+        return response
