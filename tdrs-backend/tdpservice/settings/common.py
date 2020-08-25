a05679501c354f6a4706c235e237714592ad35d5
Adam Caron <adamwcaron@gmail.com>
2020-08-25 14:37:52 -0400

    Remove default 401 response
    The frontend will call /auth_check on every page load, including
    when the user is not signed in. The endpoint should not error
    with a 401 but rather should return 200 and inform the client
    whether or not the user is authenticated. The client is always
    authorized to make this request.
    
    Handle the case of anonymous users and those who are authenticated.

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -222,9 +222,6 @@ class Common(Configuration):
             'rest_framework.renderers.JSONRenderer',
             'rest_framework.renderers.BrowsableAPIRenderer',
         ),
-        'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticated',
-        ],
         'DEFAULT_AUTHENTICATION_CLASSES': (
             'tdpservice.users.authentication.CustomAuthentication',
             'rest_framework.authentication.SessionAuthentication',
