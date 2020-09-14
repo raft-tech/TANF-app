7d2bd5de09f388e30552350a2daa88fedc660f84
raftergit <patrickgoraft@MacBook-Pro.local>
2020-09-14 15:27:19 -0400

    workaround for cf-cli installing GPG key issue

--- scripts/cf-check.sh
+++ scripts/cf-check.sh
@@ -4,9 +4,10 @@ if command -v cf /dev/null 2>&1; then
 echo The command cf is available
 else
 apt-get update
-apt-get install wget gnupg2 apt-transport-https
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-apt-get update
-apt-get install cf7-cli
+apt-get install -y apt-transport-https wget gnupg git
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+apt -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true update
+apt-get install --allow-unauthenticated -y cf7-cli
+cf --version
 fi 
