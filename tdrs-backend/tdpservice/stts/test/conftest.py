961f1e8d369d9369c36f9712dd0c429f5e0a2d9d
Carlton Smith <carl@llwebconsulting.com>
2020-10-21 12:28:24 -0400

    testing

--- tdrs-backend/tdpservice/stts/test/conftest.py
+++ tdrs-backend/tdpservice/stts/test/conftest.py
@@ -3,9 +3,15 @@
 from django.core.management import call_command
 
 import pytest
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 
 @pytest.fixture
 def stts():
     """Populate STTs."""
+    logger.info("%%%%%%%%CALLING POPULATE%%%%%%%%%%%%")
     call_command("populate_stts")
