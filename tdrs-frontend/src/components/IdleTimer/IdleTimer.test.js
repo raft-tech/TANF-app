2e0d2662b484fca5b53ef881d00e180c257508f4
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2020-11-05 09:26:07 -0800

    add tests to App and IdleTimer

--- tdrs-frontend/src/components/IdleTimer/IdleTimer.test.js
+++ tdrs-frontend/src/components/IdleTimer/IdleTimer.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { render } from '@testing-library/react'
 
+import { act } from 'react-dom/test-utils'
 import IdleTimer from './IdleTimer'
 
 describe('IdleTimer', () => {
@@ -20,39 +22,16 @@ describe('IdleTimer', () => {
     expect(modal.hasClass('display-none')).toBeTruthy()
   })
 
-  // it('should change to a className of display-block', (done) => {
-  //   const wrapper = shallow(<IdleTimer />)
+  it('should change to a className of display-block after 2 seconds', () => {
+    jest.useFakeTimers()
+    const { container } = render(<IdleTimer />)
 
-  //   const modal = wrapper.find('#myModal')
+    const modal = container.querySelector('#myModal')
 
-  //   setTimeout(() => {
-  //     console.log('timed out')
-  //     expect(modal.hasClass('display-block')).toBeTruthy()
-  //     wrapper.unmount()
-  //     done()
-  //   }, 5000)
-  // })
+    act(() => {
+      jest.runAllTimers()
+    })
 
-  // it('should setDisplay to true when page is idle for five seconds', (done) => {
-  // Cache original functionality
-  // const realUseState = React.useState
-
-  // Stub the initial state
-  // const stubInitialState = true
-
-  // Mock useState before rendering your component
-  // jest
-  //   .spyOn(React, 'useState')
-  //   .mockImplementationOnce(() => realUseState(stubInitialState))
-
-  // const wrapper = shallow(<IdleTimer />)
-
-  // React.useState(true)
-
-  // setTimeout(() => {
-  // expect(React.useState).toHaveBeenCalledTimes(1)
-  //   wrapper.unmount()
-  //   done()
-  // }, 3000)
-  // })
+    expect(modal.classList.contains('display-block')).toBeTruthy()
+  })
 })
