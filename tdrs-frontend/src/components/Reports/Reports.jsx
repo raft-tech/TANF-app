e9ed40222a411b9bfa79ddc363a391831cc4d7b2
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2020-12-02 13:24:53 -0800

    add function for creating year options in Reports,  change h2 to update on selected year, add state for selectedyear, add corresponding tests

--- tdrs-frontend/src/components/Reports/Reports.jsx
+++ tdrs-frontend/src/components/Reports/Reports.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Button'
 import { history } from '../../configureStore'
 
 function Reports() {
+  const [selectedYear, setSelectedYear] = useState(2020)
+
   const handleClick = () => {
     history.push('/reports/upload')
+  }
+
+  const handleSelect = ({ target: { value } }) => {
+    setSelectedYear(value)
+  }
+
+  const createOptions = (years) => {
+    let yearsBack = years
+    const options = []
+    let currentYear = 2020
+
+    do {
+      yearsBack -= 1
+      options.push(<option key={currentYear}>{currentYear}</option>)
+      currentYear -= 1
+    } while (yearsBack > 0)
+
+    return options
   }
   return (
     <div className="grid-container">
@@ -13,22 +33,24 @@ function Reports() {
       </h1>
       <form className="usa-form">
         {/* eslint-disable-next-line */}
-        <label className="usa-label margin-top-4" htmlFor="reports">
+        <label className="usa-label text-bold margin-top-4" htmlFor="reports">
           Reporting Year
         </label>
-
-        <select className="usa-select" name="options" id="reports">
-          <option value>- Select -</option>
-          <option value="value1">Option A</option>
-          <option value="value2">Option B</option>
-          <option value="value3">Option C</option>
+        {/* eslint-disable-next-line */}
+        <select
+          className="usa-select"
+          name="options"
+          id="reports"
+          onChange={handleSelect}
+        >
+          {createOptions(3)}
         </select>
 
         <h2 className="font-serif-xl margin-y-4 text-normal">
-          2020 TANF Reports
+          {selectedYear} TANF Reports
         </h2>
 
-        <p className="font-sans-lg margin-0 text-bold">Test Quarter</p>
+        <p className="font-sans-md margin-0 text-bold">Test Quarter</p>
         <p className="font-sans-md margin-0 margin-bottom-105">
           Upload by a date
         </p>
--- tdrs-frontend/src/components/Reports/Reports.test.js
+++ tdrs-frontend/src/components/Reports/Reports.test.js
@@ -22,7 +22,7 @@ describe('Reports', () => {
 
     const options = wrapper.find('option')
 
-    expect(options.length).toEqual(4)
+    expect(options.length).toEqual(3)
   })
 
   it('should change route to `/reports/upload` on click of `Begin Report` button', () => {
@@ -35,5 +35,29 @@ describe('Reports', () => {
     beginButton.simulate('click')
 
     expect(window.location.href.includes('/reports/upload')).toBeTruthy()
+  })
+
+  it('should update h2 when a new year is selected', () => {
+    const wrapper = shallow(<Reports />)
+
+    const select = wrapper.find('.usa-select')
+
+    select.simulate('change', {
+      target: {
+        value: 2019,
+      },
+    })
+
+    const h2 = wrapper.find('h2')
+
+    expect(h2.text()).toEqual('2019 TANF Reports')
+  })
+
+  it('should render the correct number of options', () => {
+    const wrapper = shallow(<Reports />)
+
+    const options = wrapper.find('option')
+
+    expect(options.length).toEqual(3)
   })
 })
