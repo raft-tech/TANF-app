6608c226e7e49dcbebdf3e33e2a1f8bcae2cc20a
Aaron Beavers <foamy125@gmail.com>
2020-12-07 15:30:06 -0600

    Add Generic File model

--- tdrs-backend/tdpservice/files/models.py
+++ tdrs-backend/tdpservice/files/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+"""Define file model."""
 
-# Create your models here.
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from ..users.models import User
+from ..stts.models import STT
+
+class File(models.Model):
+    """A model representing a file stored in AWS s3 via cloud.gov."""
+    slug = models.CharField(
+        unique=True,
+        max_length=256,
+        blank = False,
+        null= False
+    )
+    type = models.CharField(
+        max_length=8,
+        default="txt"
+    )
+    name = models.CharField(
+        max_length=256,
+        blank = False,
+        null= False
+    )
