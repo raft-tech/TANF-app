3bb3e046bda9cab633b9b216c2cb644eddb54e06
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2020-12-10 10:33:00 -0800

    add clearError action to upload, add names to CLEAR_ERROR and SET_FILE actions

--- tdrs-frontend/src/actions/upload.js
+++ tdrs-frontend/src/actions/upload.js
@@ -4,30 +4,39 @@ export const SET_FILE = 'SET_FILE'
 export const SET_FILE_ERROR = 'SET_FILE_ERROR'
 export const CLEAR_ERROR = 'CLEAR_ERROR'
 
-export const clearError = () => (dispatch) => {
-  dispatch({ type: CLEAR_ERROR })
+export const clearError = ({ name }) => (dispatch) => {
+  dispatch({ type: CLEAR_ERROR, payload: { name } })
 }
 
 // process.env.REACT_APP_ACCESS_KEY_ID and
 // process.env.SECRET_ACCESS_KEY need to be defined in your .env.local file.
 
-export const upload = ({ file }) => async (dispatch) => {
+export const upload = ({ file, name }) => async (dispatch) => {
   try {
     const URL = 'https://s3-us-gov-west-1.amazonaws.com'
+
     const resp = await axios.post(URL, file, {
-      access_key_id: process.env.REACT_APP_ACCESS_KEY_ID,
-      secret_access_key: process.env.SECRET_ACCESS_KEY,
-      region: 'us-gov-west-1',
-      bucket: 'cg-f073b546-cf1c-4960-845f-746318ebc15e',
+      headers: {
+        access_key_id: process.env.REACT_APP_ACCESS_KEY_ID,
+        secret_access_key: process.env.SECRET_ACCESS_KEY,
+        region: 'us-gov-west-1',
+        bucket: 'cg-f073b546-cf1c-4960-845f-746318ebc15e',
+      },
     })
 
     if (resp) {
-      dispatch({ type: SET_FILE, file: resp })
+      dispatch({
+        type: SET_FILE,
+        payload: {
+          file,
+          name,
+        },
+      })
     } else {
       console.log('NO RESPONSE')
     }
   } catch (error) {
-    dispatch({ type: SET_FILE_ERROR, payload: { error } })
+    dispatch({ type: SET_FILE_ERROR, payload: { error, name } })
     return false
   }
 
