a4e95c14f6371d97646c4c7ed96daa7bf844cefc
Aaron Beavers <foamy125@gmail.com>
2020-12-11 11:03:40 -0600

    Add report views

--- tdrs-backend/tdpservice/reports/views.py
+++ tdrs-backend/tdpservice/reports/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+"""Define API views for report files"""
+import logging
 
-# Create your views here.
+from django.utils import timezone
+
+from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .models import Report
+from .permissions import IsUserOrReadOnly
+from .serializers import ReportFileSerializer
+
+logger = logging.getLogger(__name__)
+
+
+class ReportViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    def get_serializer_class(self):
+        """Return the serializer class."""
+        return {
+            "create": CreateUserSerializer,
+            "set_profile": UserProfileSerializer,
+        }.get(self.action, ReportSerializer)
