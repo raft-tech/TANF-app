12687f329fa2ac497af97b9c5abeced88a2d4549
Aaron Beavers <foamy125@gmail.com>
2020-12-11 10:47:11 -0600

    Add enums for unique constraints on file version history

--- tdrs-backend/tdpservice/reports/models.py
+++ tdrs-backend/tdpservice/reports/models.py
@@ -77,3 +77,43 @@ class ReportFile(File):
         max_length=8,
         default="txt"
     )
+class ReportFile(File):
+    """Represents a version of a report file."""
+    class Section(models.TextChoices):
+        """Enum for report section."""
+        ACTIVE_CASE_DATA = "Active Case Data"
+        CLOSE_CASE_DATA = "Close Case Data"
+        AGGREGATE_DATA = "Aggregate Data"
+        STRATUM_DATA = "Stratum Data"
+    class Quarter(models.TextChoices):
+        """Enum for report Quarter"""
+        Q1 = "Q1"
+        Q2 = "Q2"
+        Q3 = "Q3"
+        Q4 = "Q4"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields = ("section","version","quarter","year"),
+                name = 'constraint_name')]
+
+    version = models.IntegerField()
+    year = models.IntegerField()
+    section = models.CharField(
+        max_length=200, blank=True, null=True, choices=Section.choices)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user",
+        blank=False,
+        null=False)
+    # I don't think we actually need an STT here, cause the user has an STT.
+    # I will use a serializer method to extract it from
+    # stt = models.ForeignKey(
+    #     STT,
+    #     on_delete=models.CASCADE,
+    #     related_name='stt',
+    #     blank=False,
+    #     null=False
+    # )
