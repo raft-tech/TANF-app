695f0bd41503553a2c222008b8e4533db055bc13
Aaron Beavers <foamy125@gmail.com>
2020-12-11 10:47:11 -0600

    Add enums for unique constraints on file version history

--- tdrs-backend/tdpservice/reports/models.py
+++ tdrs-backend/tdpservice/reports/models.py
@@ -1,34 +1,24 @@
 """Define report models."""
 
 from django.db import models
-from ..files.models import File
-from ..users.models import User
-from ..stts.models import STT
 
-class ReportFile(models.Model):
-    """Represents a version of a report file."""
-    name = models.CharField(
+from ..stts.models import STT
+from ..users.models import User
+
+
+# The Report File model was starting to explode, and I think that keeping this logic
+# in its own abstract class is better for documentation purposes.
+class File(models.Model):
+    """Abstract type representing a file stored in S3"""
+    class Meta:
+        abstract = True
+
+    original_filename = models.CharField(
         max_length=256,
         blank = False,
         null= False
     )
-    section = models.CharField(
-        
-    )
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="user",
-        blank=False,
-        null=False
-    )
-    stt = models.ForeignKey(
-        STT,
-        on_delete=models.CASCADE,
-        related_name='stt',
-        blank=False,
-        null=False
-    )
+
     slug = models.CharField(
         unique=True,
         max_length=256,
@@ -39,3 +29,43 @@ class ReportFile(models.Model):
         max_length=8,
         default="txt"
     )
+class ReportFile(File):
+    """Represents a version of a report file."""
+    class Section(models.TextChoices):
+        """Enum for report section."""
+        ACTIVE_CASE_DATA = "Active Case Data"
+        CLOSE_CASE_DATA = "Close Case Data"
+        AGGREGATE_DATA = "Aggregate Data"
+        STRATUM_DATA = "Stratum Data"
+    class Quarter(models.TextChoices):
+        """Enum for report Quarter"""
+        Q1 = "Q1"
+        Q2 = "Q2"
+        Q3 = "Q3"
+        Q4 = "Q4"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields = ("section","version","quarter","year"),
+                name = 'constraint_name')]
+
+    version = models.IntegerField()
+    year = models.IntegerField()
+    section = models.CharField(
+        max_length=200, blank=True, null=True, choices=Section.choices)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user",
+        blank=False,
+        null=False)
+    # I don't think we actually need an STT here, cause the user has an STT.
+    # I will use a serializer method to extract it from
+    # stt = models.ForeignKey(
+    #     STT,
+    #     on_delete=models.CASCADE,
+    #     related_name='stt',
+    #     blank=False,
+    #     null=False
+    # )
