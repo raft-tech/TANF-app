984c095645b7d96bb6549e8c8fe0f878108ee16e
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2020-12-11 10:32:54 -0800

    make form labels bold to match mocks

--- tdrs-frontend/src/components/FormGroup/FormGroup.jsx
+++ tdrs-frontend/src/components/FormGroup/FormGroup.jsx
@@ -30,7 +30,7 @@ function FormGroup({
   return (
     <div className={`usa-form-group ${error ? 'usa-form-group--error' : ''}`}>
       <label
-        className={`usa-label ${error ? 'usa-label--error' : ''}`}
+        className={`usa-label text-bold ${error ? 'usa-label--error' : ''}`}
         htmlFor={name}
       >
         {label} (required)
@@ -49,6 +49,7 @@ function FormGroup({
           onChange={({ target: { value } }) => {
             handleChange({ name, value })
           }}
+          onClick={() => console.log('formgroup was clicked')}
           onBlur={handleBlur}
         />
       </label>
