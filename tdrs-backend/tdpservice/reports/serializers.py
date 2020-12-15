052c98157be7f300ca9d5cbea74441e1ad7624f9
Aaron Beavers <foamy125@gmail.com>
2020-12-15 13:44:28 -0600

    lint

--- tdrs-backend/tdpservice/reports/serializers.py
+++ tdrs-backend/tdpservice/reports/serializers.py
@@ -1,40 +1,42 @@
 """Serialize stt data."""
 
-from rest_framework import serializers
 from django.db.models import Max
 
-from ..users.serializers import UserSerializer
-from ..stts.serializers import STTSerializer
-from .models import ReportFile
-from ..users.models import User
+from rest_framework import serializers
+
 from ..stts.models import STT
+from ..stts.serializers import STTSerializer
+from ..users.models import User
+from ..users.serializers import UserSerializer
+from .models import ReportFile
 
 
 class ReportFileSerializer(serializers.ModelSerializer):
     stt = serializers.PrimaryKeyRelatedField(queryset=STT.objects.all())
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
     class Meta:
         model = ReportFile
         fields = [
-            'original_filename',
-            'slug',
-            'extension',
-            'user',
-            'stt',
-            'year',
-            'quarter',
-            'section',
+            "original_filename",
+            "slug",
+            "extension",
+            "user",
+            "stt",
+            "year",
+            "quarter",
+            "section",
         ]
+
     def create(self, validated_data):
 
         version = 1
         latest_report = ReportFile.objects.filter(
-            slug__exact=validated_data['slug'],
-        ).aggregate(Max('version'))
+            slug__exact=validated_data["slug"],
+        ).aggregate(Max("version"))
 
-        if latest_report['version__max'] is not None:
-            version = latest_report.version + 1
-
+        if latest_report["version__max"] is not None:
+            version = latest_report["version__max"] + 1
 
         return ReportFile.objects.create(
             version=version,
