3808e95aaa92e5c47c3d19d867636f37926fecf1
Aaron Beavers <foamy125@gmail.com>
2020-12-17 16:43:20 -0600

    Add test for presigned url

--- tdrs-backend/tdpservice/reports/views.py
+++ tdrs-backend/tdpservice/reports/views.py
@@ -9,6 +9,8 @@ from .serializers import ReportFileSerializer
 from .models import User
 from .serializers import ReportFileSerializer
 
+from rest_framework.decorators import action
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -32,4 +34,24 @@ class ReportFileViewSet(
         """Return the serializer class."""
         return {
             "create": ReportFileSerializer,
+            "get_signed_url":PresignedUrlInputSerializer,
         }.get(self.action, ReportFileSerializer)
+
+    @action(methods=["POST"], detail=False)
+    def get_signed_url(self, request,pk=None):
+        s3_client = boto3.client('s3')
+        serializer = self.get_serializer(
+            self.request.user,
+            request.data,
+        )
+        try:
+            Response(s3_client.generate_presigned_url(
+                'put_object',
+                Params={
+                    'Bucket': "cg-f0e35234-e70c-491c-a044-6836dd6abd59",
+                    'Region' : "us-gov-west-1",
+                    'Key': serializer.data['file_name'],
+                }, ExpiresIn=500))
+        except ClientError as e:
+            logging.error(e)
+            return None
