1e0f65d167fe80b4d1e1972946fd067be9e3340e
Aaron Beavers <foamy125@gmail.com>
2020-12-29 13:06:03 -0500

    Converty UUID to str

--- tdrs-backend/tdpservice/reports/test/test_api.py
+++ tdrs-backend/tdpservice/reports/test/test_api.py
@@ -16,7 +16,7 @@ def test_create_report_file_entry(api_client, ofa_admin):
     data = {
         "original_filename": "report.txt",
         "quarter": "Q1",
-        "slug": uuid.uuid4(),
+        "slug": str(uuid.uuid4()),
         "user": user.id,
         "stt": user.stt.id,
         "year": 2020,
