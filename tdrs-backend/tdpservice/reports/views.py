ca35eaa26aff366cdb0d7a778b7b53dba771130b
Aaron Beavers <foamy125@gmail.com>
2020-12-29 13:06:40 -0500

    Add get report API controller

--- tdrs-backend/tdpservice/reports/views.py
+++ tdrs-backend/tdpservice/reports/views.py
@@ -3,23 +3,52 @@ import logging
 import os
 
 from rest_framework import mixins, viewsets
-
+import boto3
 from ..users.permissions import CanUploadReport
+
+from ..users.permissions import IsUserOrReadOnly
+from .serializers import ReportFileSerializer,PresignedUrlInputSerializer
+from .models import ReportFile
 from .models import User
-from .serializers import ReportFileSerializer
-from .models import User
-from .serializers import ReportFileSerializer
 
 from rest_framework.decorators import action
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
+def to_space_case(snake_str): return ''.join(x.title()+" " for x in snake_str.split('_')).strip()
+
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+
+class GetReport(APIView):
+    "Get latest version of specified report file"
+
+    query_string = False
+    pattern_name = "report"
+    permission_classes = [AllowAny]
+
+    def get(self,request,year,quarter,section):
+        print({"year":year,"quarter":quarter,"section":to_space_case(section),"stt":request.user.stt.id})
+
+        print(ReportFileSerializer(ReportFile.objects.all()[0]).data)
+
+        latest = ReportFile.find_latest_version(
+            year=year,
+            quarter=quarter,
+            section=to_space_case(section),
+            stt=request.user.stt.id)
+        print(latest)
+        serializer =   ReportFileSerializer(latest)
+        print(serializer)
+        data = serializer.data
+        return Response(data,template_name="report.json")
+
 class ReportFileViewSet(
     mixins.CreateModelMixin, viewsets.GenericViewSet,
 ):
     """Report file views."""
-
     queryset = User.objects.select_related("stt")
 
     def get_permissions(self):
