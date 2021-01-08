2108ab67172db4a95e60d3e71e77c65fe43446ef
Aaron Beavers <foamy125@gmail.com>
2021-01-08 16:59:58 -0500

    Update imports, remove unnecessary commented class

--- tdrs-backend/tdpservice/reports/views.py
+++ tdrs-backend/tdpservice/reports/views.py
@@ -4,36 +4,12 @@ import logging
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import AllowAny
 
-from ..users.permissions import IsUserOrReadOnly
+from ..users.permissions import  IsOFAAnalyst, IsOFAAdmin, IsUserOrAdmin,IsOFA
 from .models import User
 from .serializers import ReportFileSerializer
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-
-# class AuthorizationCheck(APIView):
-#     """Check if user is authorized."""
-
-#     query_string = True
-#     pattern_name = "authorization-check"
-#     permission_classes = [AllowAny] # What should I set this to?
-
-#     def post(self, request, *args, **kwargs):
-#         """Handle get request and verify user is authorized."""
-#         user = request.user
-#         serializer = ReportFileSerializer(request.data)
-#         if user.is_authenticated:
-#             logger.info(
-#                 "Auth check PASS for user: %s on %s", user.username, timezone.now()
-#             )
-#             res = Response({"success":True})
-#             res['Access-Control-Allow-Headers'] = "X-CSRFToken"
-#             return res
-#         else:
-#             logger.info("Auth check FAIL for user on %s", timezone.now())
-#             return Response({"authenticated": False})
-
 
 class ReportFileViewSet(
     mixins.CreateModelMixin,
