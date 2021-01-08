15e171d3daf14828017edec367af3c51af73f8be
Aaron Beavers <foamy125@gmail.com>
2021-01-08 17:13:35 -0500

    Add new imports, add is_in_group utility,  comment question

--- tdrs-backend/tdpservice/users/permissions.py
+++ tdrs-backend/tdpservice/users/permissions.py
@@ -1,8 +1,18 @@
 """Set permissions for users."""
 
 from rest_framework import permissions
+from django.contrib.auth.models import Group
 
+def is_in_group(user, group_name):
+    """
+    Takes a user and a group name, and returns `True` if the user is in that group.
+    """
+    try:
+        return Group.objects.get(name=group_name).user_set.filter(id=user.id).exists()
+    except Group.DoesNotExist:
+        return None
 
+# Is Owner or admin?
 class IsUserOrAdmin(permissions.BasePermission):
     """Object-level permission to only allow owners of an object to edit it."""
 
