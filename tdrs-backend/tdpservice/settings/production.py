3341dce28f18c63431215761acedff213dbe20d6
Carlton Smith <carl@llwebconsulting.com>
2021-01-13 16:09:25 -0500

    staging and prod keys

--- tdrs-backend/tdpservice/settings/production.py
+++ tdrs-backend/tdpservice/settings/production.py
@@ -20,9 +20,16 @@ class Production(Common):
     INSTALLED_APPS += ("storages",)
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    # static files s3 bucket
     AWS_ACCESS_KEY_ID = os.getenv("DJANGO_AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("DJANGO_AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("DJANGO_AWS_STORAGE_BUCKET_NAME")
+    # data files s3 bucket
+    AWS_S3_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY")
+    AWS_S3_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+
+    AWS_REGION_NAME = os.getenv("AWS_REGION_NAME")
     AWS_DEFAULT_ACL = "public-read"
     AWS_AUTO_CREATE_BUCKET = True
     AWS_QUERYSTRING_AUTH = False
