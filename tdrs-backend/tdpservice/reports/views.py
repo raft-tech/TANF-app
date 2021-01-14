f1d61e65b965f025bc587260d36225f04f5e6907
Aaron Beavers <foamy125@gmail.com>
2021-01-14 16:11:46 -0500

    Use new permission for report model post

--- tdrs-backend/tdpservice/reports/views.py
+++ tdrs-backend/tdpservice/reports/views.py
@@ -3,13 +3,12 @@ import logging
 
 from rest_framework import mixins, viewsets
 
-from ..users.permissions import IsOFA
+from ..users.permissions import CanUploadReport
 from .models import User
 from .serializers import ReportFileSerializer
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 
 class ReportFileViewSet(
     mixins.CreateModelMixin, viewsets.GenericViewSet,
@@ -20,7 +19,7 @@ class ReportFileViewSet(
 
     def get_permissions(self):
         """Get permissions for the viewset."""
-        permission_classes = {"create": [IsOFA]}.get(self.action)
+        permission_classes = {"create": [CanUploadReport]}.get(self.action)
         return [permission() for permission in permission_classes]
 
     def get_serializer_class(self):
