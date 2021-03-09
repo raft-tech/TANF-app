2e9d63c5825d1aff9a5ba544ec797e611a0f5399
Aaron Beavers <foamy125@gmail.com>
2021-03-09 12:34:10 -0500

    Update tdrs-backend/tdpservice/reports/test/test_api.py
    Co-authored-by: John Willis <jtwillis92@gmail.com>

--- tdrs-backend/tdpservice/reports/test/test_api.py
+++ tdrs-backend/tdpservice/reports/test/test_api.py
@@ -121,14 +121,15 @@ def test_reports_data_prepper_not_allowed(api_client, data_prepper):
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 @pytest.mark.django_db
-def test_s3_signed_url(api_client, user):
-    """Test that a url string is given to the client."""
-    api_client.login(username=user.username, password="test_password")
-    response = api_client.post("/v1/reports/signed_url/", {
-        "file_name": "test.txt",
-        "file_type": "plain/text",
-        "client_method": "put_object"
-    })
+@pytest.mark.parametrize('client_method', ['get_object', 'put_object'])
+def test_s3_signed_url(api_client, client_method, user):
+   """Test that a url string is given to the client."""
+   api_client.login(username=user.username, password="test_password")
+   response = api_client.post("/v1/reports/signed_url/", {
+       "file_name": "test.txt",
+       "file_type": "plain/text",
+       "client_method": client_method
+   })
 
-    assert response.status_code == status.HTTP_200_OK
-    assert response.data['signed_url']
+   assert response.status_code == status.HTTP_200_OK
+   assert response.data['signed_url']
