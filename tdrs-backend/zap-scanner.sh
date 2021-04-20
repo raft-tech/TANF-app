1162282264fa162fe735cc735d3f3b60fda76b10
Carlton Smith <carl@llwebconsulting.com>
2021-04-20 10:19:33 -0400

    remove container and -v

--- tdrs-backend/zap-scanner.sh
+++ tdrs-backend/zap-scanner.sh
@@ -9,7 +9,6 @@ fi
 docker-compose down
 docker-compose up -d --build
 	# do an OWASP ZAP scan
-	CONTAINER=$(docker-compose images | awk '/zaproxy/ {print $1}')
  	export ZAP_CONFIG=" \
 	  -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
 	  -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
@@ -18,7 +17,6 @@ docker-compose up -d --build
 
 	echo "================== OWASP ZAP tests =================="
 	docker-compose exec zaproxy zap-full-scan.py \
-	  -v $(pwd)/reports:/zap/wrk:rw \
 	  -t http://web:8080/ \
 	  -m 5 \
 	  -z "${ZAP_CONFIG}" \
