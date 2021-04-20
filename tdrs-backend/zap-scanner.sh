5935150dc565fbae5cb913794aa20983306c7a46
Carlton Smith <carl@llwebconsulting.com>
2021-04-20 11:14:00 -0400

    refactor again

--- tdrs-backend/zap-scanner.sh
+++ tdrs-backend/zap-scanner.sh
@@ -8,22 +8,26 @@ fi
 
 docker-compose down
 docker-compose up -d --build
-	# do an OWASP ZAP scan
- 	export ZAP_CONFIG=" \
-	  -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
-	  -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
-	  -config globalexcludeurl.url_list.url\(0\).enabled=true \
-	  -config spider.postform=true"
+    # do an OWASP ZAP scan
+     export ZAP_CONFIG=" \
+      -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
+      -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
+      -config globalexcludeurl.url_list.url\(0\).enabled=true \
+      -config spider.postform=true"
 
-	echo "================== OWASP ZAP tests =================="
-	docker-compose run zaproxy zap-full-scan.py -t http://web:8080/ -m 5 \
-	  -z "${ZAP_CONFIG}" -r owasp_report.html | grep -q "FAIL-NEW: 0"
-	ZAPEXIT=$?
+    echo "================== OWASP ZAP tests =================="
+    docker-compose run zaproxy zap-full-scan.py \
+        -t http://web:8080/ \
+        -m 5 \
+        -z "${ZAP_CONFIG}" \
+        -r owasp_report.html | tee /dev/tty | grep -q "FAIL-NEW: 0"
+    ZAPEXIT=$?
+
 EXIT=0
 
 if [ "$ZAPEXIT" = 1 ] ; then
-	echo "OWASP ZAP scan failed"
-	EXIT=1
+    echo "OWASP ZAP scan failed"
+    EXIT=1
 fi
 
 docker-compose down --remove-orphan
