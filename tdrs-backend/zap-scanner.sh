12560235f0b1756060cd1b7666816d46f9ac71b8
Carlton Smith <carl@llwebconsulting.com>
2021-04-20 11:02:46 -0400

    new line

--- tdrs-backend/zap-scanner.sh
+++ tdrs-backend/zap-scanner.sh
@@ -17,7 +17,8 @@ docker-compose up -d --build
 
 	echo "================== OWASP ZAP tests =================="
 	docker-compose run zaproxy zap-full-scan.py -t http://web:8080/ -m 5 \
-	  -z "${ZAP_CONFIG}" -r owasp_report.html | grep -q "FAIL-NEW: 0" ZAPEXIT=$?
+	  -z "${ZAP_CONFIG}" -r owasp_report.html | grep -q "FAIL-NEW: 0"
+	ZAPEXIT=$?
 EXIT=0
 
 if [ "$ZAPEXIT" = 1 ] ; then
