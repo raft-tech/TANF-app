a720184ffcb632ce75f646ab14ce4f46ff575a17
Jorge Gonzalez <jorgegonzalez@users.noreply.github.com>
2022-03-16 10:48:22 -0400

    fix: migration

--- tdrs-backend/tdpservice/users/migrations/0033_user_nextgen_xid.py
+++ tdrs-backend/tdpservice/users/migrations/0033_user_nextgen_xid.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0031_remove_federal_gov_stt'),
+        ('users', '0032_ocio_permissions_group'),
     ]
 
     operations = [
