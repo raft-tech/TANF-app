105cc1b098d0be011e0382e027cc2bc59decd3ef
raftmsohani <msohani@goraft.tech>
2022-05-05 05:36:47 -0700

    Renamed migration conflics

--- tdrs-backend/tdpservice/users/migrations/0034_user_nextgen_xid.py
+++ tdrs-backend/tdpservice/users/migrations/0034_user_nextgen_xid.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0032_ocio_permissions_group'),
+        ('users', '0033_alter_user_hhs_id'),
     ]
 
     operations = [
