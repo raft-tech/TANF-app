932ac3c71b4e266b520f515f2f7eae310dcb3ac6
Aaron Beavers <foamy125@gmail.com>
2022-07-21 15:10:36 -0400

    tough as a cookie

--- tdrs-frontend/src/axios-instance.js
+++ tdrs-frontend/src/axios-instance.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import tough from 'tough-cookie'
+import axiosCookieJarSupport from 'axios-cookiejar-support'
+axiosCookieJarSupport(axios)
 
 // Need a custom instance of axios so we can set the csrf keys on auth_check
 // Work around for csrf cookie issue we encountered in production.
@@ -6,4 +9,7 @@ import axios from 'axios'
 // frontend (most likely) is misconfigured. the configuration has alluded
 // us thus far, and this implementation is functionally equivalent to
 // using cookies.
-export default axios.create()
+export default axios.create({
+  jar:new tough.CookieJar(),
+  withCredentials:true
+})
