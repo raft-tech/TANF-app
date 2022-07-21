b23a6fb23ac9670745534c26ab1514d57f485368
Aaron Beavers <foamy125@gmail.com>
2022-07-21 11:53:24 -0400

    add a few allowed hearders

--- tdrs-backend/tdpservice/users/api/authorization_check.py
+++ tdrs-backend/tdpservice/users/api/authorization_check.py
@@ -1,14 +1,16 @@
 """Check if user is authorized."""
 
 import logging
+
 from django.contrib.auth import logout
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
 from django.middleware import csrf
 from django.utils import timezone
-from ..serializers import UserProfileSerializer
 
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..serializers import UserProfileSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -43,7 +45,7 @@ class AuthorizationCheck(APIView):
                 "Auth check PASS for user: %s on %s", user.username, timezone.now()
             )
             res = Response(auth_params)
-            res["Access-Control-Allow-Headers"] = "X-CSRFToken"
+            res["Access-Control-Allow-Headers"] = "X-CSRFToken, X-XSRF-Token, XSRF-Token"
             return res
         else:
             logger.info("Auth check FAIL for user on %s", timezone.now())
