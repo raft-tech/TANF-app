266366cd9341c15327bfe59b4c3b2c7d20da2e96
Jan Timpe <jtimpe@goraft.tech>
2022-09-21 08:27:42 -0400

    fix elastic impl, configure remaining submission models

--- tdrs-backend/tdpservice/search_indexes/apps.py
+++ tdrs-backend/tdpservice/search_indexes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SearchIndexesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'search_indexes'
+    name = 'tdpservice.search_indexes'
