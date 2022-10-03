e24b099aa9f52700635a0bc574797dd4af2eff3e
Cameron Smart <smartcameron@gmail.com>
2022-10-03 15:56:39 -0700

    Set up emails for account deactivated.

--- tdrs-backend/tdpservice/users/models.py
+++ tdrs-backend/tdpservice/users/models.py
@@ -15,46 +15,50 @@ from django.utils.translation import gettext_lazy as _
 
 logger = logging.getLogger()
 
+
 class AccountApprovalStatusChoices(models.TextChoices):
     """Enum of options for `account_approval_status`."""
 
-    INITIAL = 'Initial'
-    ACCESS_REQUEST = 'Access request'
-    PENDING = 'Pending'
-    APPROVED = 'Approved'
-    DENIED = 'Denied'
-    DEACTIVATED = 'Deactivated'
+    INITIAL = "Initial"
+    ACCESS_REQUEST = "Access request"
+    PENDING = "Pending"
+    APPROVED = "Approved"
+    DENIED = "Denied"
+    DEACTIVATED = "Deactivated"
 
     # is "pending", "approved", and "denied" enough to cover functionality?
+
 
 class User(AbstractUser):
     """Define user fields and methods."""
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    limit = models.Q(app_label='stts', model='stt') | models.Q(app_label='stts', model='region')
+    limit = models.Q(app_label="stts", model="stt") | models.Q(
+        app_label="stts", model="region"
+    )
 
     location_id = models.PositiveIntegerField(null=True, blank=True)
-    location_type = models.ForeignKey(ContentType,
-                                      on_delete=models.CASCADE,
-                                      null=True, blank=True,
-                                      limit_choices_to=limit)
-    location = GenericForeignKey('location_type', 'location_id')
+    location_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        limit_choices_to=limit,
+    )
+    location = GenericForeignKey("location_type", "location_id")
 
     # The unique `sub` UUID from decoded login.gov payloads for login.gov users.
-    login_gov_uuid = models.UUIDField(editable=False,
-                                      blank=True,
-                                      null=True,
-                                      unique=True)
+    login_gov_uuid = models.UUIDField(
+        editable=False, blank=True, null=True, unique=True
+    )
 
     # Unique `hhsid` user claim for AMS OpenID users.
     # In the future, `TokenAuthorizationAMS.get_auth_options` will use `hhs_id` as the primary auth field.
     # See also: CustomAuthentication.py
-    hhs_id = models.CharField(editable=False,
-                              max_length=12,
-                              blank=True,
-                              null=True,
-                              unique=True)
+    hhs_id = models.CharField(
+        editable=False, max_length=12, blank=True, null=True, unique=True
+    )
 
     # deprecated: use `account_approval_status`
     # Note this is handled differently than `is_active`, which comes from AbstractUser.
@@ -62,12 +66,12 @@ class User(AbstractUser):
     # This field `deactivated` helps us to notify the user client-side of their status
     # with an "Inactive Account" message.
     deactivated = models.BooleanField(
-        _('deactivated'),
+        _("deactivated"),
         default=False,
         help_text=_(
-            'Deprecated: use Account Approval Status instead - '
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
+            "Deprecated: use Account Approval Status instead - "
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
         ),
     )
 
@@ -77,9 +81,9 @@ class User(AbstractUser):
     access_request = models.BooleanField(
         default=False,
         help_text=_(
-            'Deprecated: use Account Approval Status instead - '
-            'Designates whether this user account has requested access to TDP. '
-            'Users with this checked must have groups assigned for the application to work correctly.'
+            "Deprecated: use Account Approval Status instead - "
+            "Designates whether this user account has requested access to TDP. "
+            "Users with this checked must have groups assigned for the application to work correctly."
         ),
     )
 
@@ -91,9 +95,9 @@ class User(AbstractUser):
         choices=AccountApprovalStatusChoices.choices,
         default=AccountApprovalStatusChoices.INITIAL,
         help_text=_(
-            'Designates whether this user account is active and approved to access TDP. '
-            'Users in an approved state are allowed access.'
-        )
+            "Designates whether this user account is active and approved to access TDP. "
+            "Users in an approved state are allowed access."
+        ),
     )
 
     def __str__(self):
@@ -106,15 +110,30 @@ class User(AbstractUser):
 
     def validate_location(self):
         """Throw a validation error if a user has a location type incompatable with their role."""
-        if (not (self.is_regional_staff or self.is_data_analyst or self.is_developer)) and self.location:
+        if (
+            not (self.is_regional_staff or self.is_data_analyst or self.is_developer)
+        ) and self.location:
             raise ValidationError(
-                _("Users other than Regional Staff, Developers, Data Analysts do not get assigned a location"))
-        elif self.is_regional_staff and self.location_type and self.location_type.model != 'region':
+                _(
+                    "Users other than Regional Staff, Developers, Data Analysts do not get assigned a location"
+                )
+            )
+        elif (
+            self.is_regional_staff
+            and self.location_type
+            and self.location_type.model != "region"
+        ):
             raise ValidationError(
-                _("Regional staff cannot have a location type other than region"))
-        elif self.is_data_analyst and self.location_type and self.location_type.model != 'stt':
+                _("Regional staff cannot have a location type other than region")
+            )
+        elif (
+            self.is_data_analyst
+            and self.location_type
+            and self.location_type.model != "stt"
+        ):
             raise ValidationError(
-                _("Data Analyst cannot have a location type other than stt"))
+                _("Data Analyst cannot have a location type other than stt")
+            )
 
     def clean(self, *args, **kwargs):
         """Prevent save if attributes are not necessary for a user given their role."""
@@ -134,17 +153,17 @@ class User(AbstractUser):
     @property
     def is_data_analyst(self) -> bool:
         """Return whether or not the user is in the Data Analyst Group."""
-        return self.is_in_group('Data Analyst')
+        return self.is_in_group("Data Analyst")
 
     @property
     def is_ocio_staff(self) -> bool:
         """Return whether or not the user is in the ACF OCIO Group."""
-        return self.is_in_group('ACF OCIO')
+        return self.is_in_group("ACF OCIO")
 
     @property
     def stt(self):
         """Return the location if the location is an STT."""
-        if self.location and self.location_type.model == 'stt':
+        if self.location and self.location_type.model == "stt":
             return self.location
         else:
             return None
@@ -153,13 +172,14 @@ class User(AbstractUser):
     def stt(self, value):
         if self.is_regional_staff:
             raise ValidationError(
-                _("Regional staff cannot have an sst assigned to them"))
+                _("Regional staff cannot have an sst assigned to them")
+            )
         self.location = value
 
     @property
     def region(self):
         """Return the location if the location is a Region."""
-        if self.location and self.location_type.model == 'region':
+        if self.location and self.location_type.model == "region":
             return self.location
         else:
             return None
@@ -168,7 +188,8 @@ class User(AbstractUser):
     def region(self, value):
         if self.is_data_analyst:
             raise ValidationError(
-                _("Data Analyst cannot have a region assigned to them"))
+                _("Data Analyst cannot have a region assigned to them")
+            )
         self.location = value
 
     @property
@@ -176,16 +197,14 @@ class User(AbstractUser):
         """Check if the user's account status has been set to 'Deactivated'."""
         return self.account_approval_status == AccountApprovalStatusChoices.DEACTIVATED
 
-
     @classmethod
     def from_db(cls, db, field_names, values):
-        """
-        Override the django Model from_db method.
+        """Override the django Model from_db method.
+
         Populates instances of User with a `_loaded_values` member,
         useful for accessing current values for the object before updates
         https://docs.djangoproject.com/en/4.1/ref/models/instances/#customizing-model-loading
         """
-
         instance = super().from_db(db, field_names, values)
         instance._state.adding = False
         instance._state.db = db
@@ -193,14 +212,13 @@ class User(AbstractUser):
         return instance
 
     def save(self, *args, **kwargs):
-        """
-        Override the django Model save method.
+        """Override the django Model save method.
+
         The existing values can be accessed using `self._loaded_values`
         which are set by `from_db`
         """
-
         if not self._state.adding:
-            current_status = self._loaded_values['account_approval_status']
+            current_status = self._loaded_values["account_approval_status"]
             new_status = self.account_approval_status
 
             if new_status != current_status:
@@ -210,22 +228,57 @@ class User(AbstractUser):
 
                 match new_status:
                     case AccountApprovalStatusChoices.INITIAL:
-                        print('initial')
+                        print("initial")
                         pass
                     case AccountApprovalStatusChoices.ACCESS_REQUEST:
-                        mail.delay(email_path=EmailType.ACCESS_REQUEST_SUBMITTED.value, recipient_email=self.email, subject="Account requested", email_context={'first_name': self.first_name, 'stt_name': str(self.stt), 'text_message': 'Your account has been requested.'})
+                        mail.delay(
+                            email_path=EmailType.ACCESS_REQUEST_SUBMITTED.value,
+                            recipient_email=self.email,
+                            subject="Account requested",
+                            email_context={
+                                "first_name": self.first_name,
+                                "stt_name": str(self.stt),
+                                "text_message": "Your account has been requested.",
+                            },
+                        )
                         pass
                     case AccountApprovalStatusChoices.PENDING:
-                        print('pending')
+                        print("pending")
                         pass
                     case AccountApprovalStatusChoices.APPROVED:
-                        mail.delay(email_path=EmailType.REQUEST_APPROVED.value, recipient_email=self.email, subject="Request approved", email_context={'first_name': self.first_name, 'stt_name': str(self.stt), 'group_permission': str(self.groups.first()), 'text_message': 'Your account request has been approved.'})
+                        mail.delay(
+                            email_path=EmailType.REQUEST_APPROVED.value,
+                            recipient_email=self.email,
+                            subject="Request approved",
+                            email_context={
+                                "first_name": self.first_name,
+                                "stt_name": str(self.stt),
+                                "group_permission": str(self.groups.first()),
+                                "text_message": "Your account request has been approved.",
+                            },
+                        )
                         pass
                     case AccountApprovalStatusChoices.DENIED:
-                        mail.delay(email_path=EmailType.REQUEST_DENIED.value, recipient_email=self.email, subject="Request denied", email_context={'first_name': self.first_name, 'stt_name': str(self.stt), 'group_permission': str(self.groups.first()), 'text_message': 'Your account request has been approved.'})
+                        mail.delay(
+                            email_path=EmailType.REQUEST_DENIED.value,
+                            recipient_email=self.email,
+                            subject="Request denied",
+                            email_context={
+                                "first_name": self.first_name,
+                                "text_message": "Your account request has been denied.",
+                            },
+                        )
                         pass
                     case AccountApprovalStatusChoices.DEACTIVATED:
-                        print('deactivated')
+                        mail.delay(
+                            email_path=EmailType.REQUEST_DENIED.value,
+                            recipient_email=self.email,
+                            subject="Request denied",
+                            email_context={
+                                "first_name": self.first_name,
+                                "text_message": "Your account has been deactivated.",
+                            },
+                        )
                         pass
                 return
 
