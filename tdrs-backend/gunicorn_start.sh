553ff80e45b441cdbd4c0a8a96ddcc2d669fe8f9
raftmsohani <msohani@goraft.tech>
2022-10-04 08:16:15 -0400

    local containerized nginx + Nginx

--- tdrs-backend/gunicorn_start.sh
+++ tdrs-backend/gunicorn_start.sh
@@ -13,6 +13,7 @@ else
     gunicorn_params="--bind 0.0.0.0:8080 --timeout 10 --workers 3 --log-level $LOGGING_LEVEL"
 fi
 
-gunicorn_cmd="gunicorn tdpservice.wsgi:application $gunicorn_params"
+# gunicorn_cmd="gunicorn tdpservice.wsgi:application $gunicorn_params"
+gunicorn_cmd="gunicorn -c gunicorn.py --log-file=-"
 
 exec $gunicorn_cmd
