3bab347cdec274a9c2c159ded8c26139de061d8e
Jan Timpe <jtimpe@goraft.tech>
2022-11-14 14:28:15 -0500

    use permission guard for data file page

--- tdrs-frontend/src/components/Routes/Routes.js
+++ tdrs-frontend/src/components/Routes/Routes.js
@@ -41,7 +41,10 @@ const AppRoutes = () => {
         exact
         path="/data-files"
         element={
-          <PrivateRoute title="Data Files">
+          <PrivateRoute
+            title="Data Files"
+            requiredPermissions={['view_datafile', 'add_datafile']}
+          >
             <Reports />
           </PrivateRoute>
         }
