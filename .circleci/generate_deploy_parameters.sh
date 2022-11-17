1f6e6378e48a85e11357a07961eb422210f8ca84
Cameron Smart <smartcameron@gmail.com>
2022-11-17 12:10:36 -0800

    set up generate parameters script

---
+++ .circleci/generate_deploy_parameters.sh
@@ -0,0 +1,8 @@
+IS_DEV_DEPLOYMENT=${1}
+TARGET_ENV=${2}
+CIRCLE_CI_BRANCH=${3}
+
+# echo the above variables to the console for debugging
+echo "IS_DEV_DEPLOYMENT: $IS_DEV_DEPLOYMENT"
+echo "TARGET_ENV: $TARGET_ENV"
+echo "CIRCLE_CI_BRANCH: $CIRCLE_CI_BRANCH"
