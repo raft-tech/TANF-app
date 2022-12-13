124311911bee004b99259f103469317e3d7cb806
Andrew Jameson <ajameson@goraft.tech>
2022-12-13 11:54:52 -0500

    commiting some experimental test work so i can rebase/merge/pull/push

---
+++ tdrs-backend/tdpservice/search_indexes/parsers/test/factories.py
@@ -0,0 +1,6 @@
+"""Generate test data files for parsers."""
+import factory
+
+# from tdpservice.users.test.factories import UserFactory
+# from tdpservice.stts.test.factories import STTFactory
+from tdpservice.data_files.test.factories import DataFileFactory
