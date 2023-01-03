296b75e07d30f2735c779c096537f84c7344bbe2
Andrew Jameson <ajameson@goraft.tech>
2023-01-03 15:45:30 -0500

    lint fixes

--- tdrs-backend/tdpservice/search_indexes/parsers/schema_defs/universal.py
+++ tdrs-backend/tdpservice/search_indexes/parsers/schema_defs/universal.py
@@ -1,15 +1,13 @@
 """A commonly-shared set of definitions for line schemas in datafiles."""
 
 def get_header_schema():
-    '''Returns the schema dictionary for the header record.
-
-    DEFINITION SOURCE:
+    """Return the schema dictionary for the header record."""
+    """DEFINITION SOURCE:
     https://www.acf.hhs.gov/sites/default/files/documents/ofa/transmission_file_header_trailer_record.pdf
 
     DESCRIPTION		LENGTH	FROM	TO	COMMENT
     Title		    6	1	6	Value	=	HEADER
     YYYYQ	        5	7	11	Value	=	YYYYQ
-
     Type 	        1	12	12	A=Active;	C=Closed;	G=Aggregate,	S=Stratum
     State Fips	    2	13	14	"2	digit	state	code	000	a	tribe"
     Tribe Code	    3	15	17	"3	digit	tribe	code	000	a	state"
@@ -24,27 +22,26 @@ def get_header_schema():
         Q=4	(Oct-Dec)
     Example:
     HEADERYYYYQTFIPSSP1EN
-    '''
+    """
     header_schema = {
             'title':        {'type': 'string', 'required': True, 'allowed': ['HEADER']},
-            'year':         {'type': 'string', 'required': True, 'regex': '^20[0-9]{2}$'},          # 4 digits, starts with 20
+            'year':         {'type': 'string', 'required': True, 'regex': '^20[0-9]{2}$'},  # 4 digits, starts with 20
             'quarter':      {'type': 'string', 'required': True, 'allowed': ['1', '2', '3', '4']},
             'type':         {'type': 'string', 'required': True, 'allowed': ['A', 'C', 'G', 'S']},
-            'state_fips':   {'type': 'string', 'required': True, 'regex': '^[0-9]{2}$'},            # 2 digits
-            'tribe_code':   {'type': 'string', 'required': False, 'regex': '^([0-9]{3}|[ ]{3})$'},  # 3 digits or 3 spaces
+            'state_fips':   {'type': 'string', 'required': True, 'regex': '^[0-9]{2}$'},  # 2 digits
+            'tribe_code':   {'type': 'string', 'required': False, 'regex': '^([0-9]{3}|[ ]{3})$'},  # digits or spaces
             'program_type': {'type': 'string', 'required': True, 'allowed': ['TAN', 'SSP']},
             'edit':         {'type': 'string', 'required': True, 'allowed': ['1', '2']},
             'encryption':   {'type': 'string', 'required': True, 'allowed': ['E', ' ']},
             'update':       {'type': 'string', 'required': True, 'allowed': ['N', 'D', 'U']},
         }
-        # we're limited to string for all values otherwise we have to cast to int in header and it fails
-        # with a raised ValueError instead of in the validator.errors
+    # we're limited to string for all values otherwise we have to cast to int in header and it fails
+    # with a raised ValueError instead of in the validator.errors
     return header_schema
 
 def get_trailer_schema():
-    '''Returns the schema dictionary for the trailer record.
-
-    DEFINITION SOURCE:
+    """Return the schema dictionary for the trailer record."""
+    """DEFINITION SOURCE:
     https://www.acf.hhs.gov/sites/default/files/documents/ofa/transmission_file_header_trailer_record.pdf
     length of 24
     DESCRIPTION LENGTH  FROM    TO  COMMENT
@@ -53,7 +50,7 @@ def get_trailer_schema():
     Blank       9       15      23  Value = spaces
     Example:
     'TRAILER0000001         '
-    '''
+    """
     trailer_schema = {
         'title':        {'type': 'string', 'required': True, 'allowed': ['TRAILER']},
         'record_count': {'type': 'string', 'required': True, 'regex': '^[0-9]{7}$'},
