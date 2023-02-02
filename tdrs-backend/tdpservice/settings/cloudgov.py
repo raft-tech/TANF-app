33267bb16575eaa6a1383f1c2b236d3239a9aec1
mo sohani <msohani@goraft.tech>
2023-02-02 15:36:28 -0500

    linting

--- tdrs-backend/tdpservice/settings/cloudgov.py
+++ tdrs-backend/tdpservice/settings/cloudgov.py
@@ -88,12 +88,12 @@ class CloudGov(Common):
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'sqlite3.db',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
         }
     }
 
