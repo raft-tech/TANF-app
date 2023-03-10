8731b938269a8e39ad75241527a1c8dff9924376
Cameron Smart <smartcameron@gmail.com>
2023-03-10 07:54:48 -0800

    added comments CCI token

--- commands.sh
+++ commands.sh
@@ -90,6 +90,7 @@ alias tdrs-fix-django-populate='tdrs-backend-exec && sed -i "s/raise Runtime..po
 alias tdrs-fresh-start='tdrs-stop && docker system prune --volumes && tdrs-start'
 
 # Deploy current branch to the given environment
+# See comments in main function for to set up Circle CI token
 alias tdrs-deploy='tdrs-run-deploy'
 
 # run flake8 against backend source from inside of web container
@@ -187,6 +188,9 @@ tdrs-run-frontend-owasp() {
 }
 
 tdrs-run-deploy() {
+    # Circle CI token can be generated here: https://app.circleci.com/settings/user/tokens
+    # Once generated add it to your shell profile as CIRCLE_CI_TOKEN
+    # Use like: tdrs-deploy sandbox
     TARGET_ENV=$1
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
