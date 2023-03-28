0e6741a0071847d808e82594939663d0d03f92d8
Andrew <84722778+andrew-jameson@users.noreply.github.com>
2023-03-28 14:30:49 -0400

    hotfix, fake the user-agent so we don't get 403

--- scripts/cf-check.sh
+++ scripts/cf-check.sh
@@ -7,7 +7,7 @@ else
     apt-get update
     apt-get install wget gnupg2 apt-transport-https
 
-    wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+    wget -U "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36" -q -O https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 
     echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 
