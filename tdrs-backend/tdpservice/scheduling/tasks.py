06037747197d17ed8e63b086fcfcf048ecb50dc4
elipe17 <elipe@teamraft.com>
2023-05-31 11:24:17 -0600

    - test code for sandbox

--- tdrs-backend/tdpservice/scheduling/tasks.py
+++ tdrs-backend/tdpservice/scheduling/tasks.py
@@ -30,19 +30,19 @@ def check_for_accounts_needing_deactivation_warning():
     deactivate_in_3_days = users_to_deactivate(3)
     deactivate_in_1_day = users_to_deactivate(1)
 
-    if deactivate_in_10_days:
-        send_deactivation_warning_email(deactivate_in_10_days, 10)
-    if deactivate_in_3_days:
-        send_deactivation_warning_email(deactivate_in_3_days, 3)
-    if deactivate_in_1_day:
-        send_deactivation_warning_email(deactivate_in_1_day, 1)
+    # if deactivate_in_10_days:
+    send_deactivation_warning_email(deactivate_in_10_days, 10)
+    # if deactivate_in_3_days:
+    #     send_deactivation_warning_email(deactivate_in_3_days, 3)
+    # if deactivate_in_1_day:
+    #     send_deactivation_warning_email(deactivate_in_1_day, 1)
 
 def users_to_deactivate(days):
     """Return a list of users that have not logged in in the last {180 - days} days."""
     days = 180 - days
     return User.objects.filter(
-        last_login__lte=datetime.now(tz=timezone.utc) - timedelta(days=days),
-        last_login__gte=datetime.now(tz=timezone.utc) - timedelta(days=days+1),
+        # last_login__lte=datetime.now(tz=timezone.utc) - timedelta(days=days),
+        # last_login__gte=datetime.now(tz=timezone.utc) - timedelta(days=days+1),
         account_approval_status=AccountApprovalStatusChoices.APPROVED,
         )
 
