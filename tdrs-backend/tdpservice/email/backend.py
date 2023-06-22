41fcee54ba76d6435f4dd34fdc2b344d91944065
Jan Timpe <jtimpe@goraft.tech>
2023-06-22 13:50:20 -0400

    impl dkim in EmailBackend override

---
+++ tdrs-backend/tdpservice/email/backend.py
@@ -0,0 +1,41 @@
+import smtplib
+from django.core.mail.backends.smtp import EmailBackend
+from django.conf import settings
+from django.core.mail.message import sanitize_address
+from dkimpy import DKIM
+
+
+class DKIMEmailBackend(EmailBackend):
+    """Override EmailBackend._save to inject a DKIM signature."""
+    def _send(self, email_message):
+        """A helper method that does the actual sending."""
+        if not email_message.recipients():
+            return False
+        encoding = email_message.encoding or settings.DEFAULT_CHARSET
+        from_email = sanitize_address(email_message.from_email, encoding)
+        recipients = [
+            sanitize_address(addr, encoding) for addr in email_message.recipients()
+        ]
+        message = email_message.message().as_string()
+
+        dkim = DKIM()
+        # signature = "dkim signature from message_string"
+        signature = dkim.sign(
+            message,
+            settings.EMAIL_DKIM_DOMAIN,
+            settings.EMAIL_DKIM_PRIVATE_KEY,
+            # signature_algorithm=None,
+            # identity=None,
+            # canonicalize=('relaxed', 'simple'),
+            # include_headers=None,
+            # length=False
+        )
+        try:
+            self.connection.sendmail(
+                from_email, recipients, signature+message
+            )
+        except smtplib.SMTPException:
+            if not self.fail_silently:
+                raise
+            return False
+        return True
