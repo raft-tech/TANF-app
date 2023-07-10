49945bc7a093b02a04948add24ca3f46f0aadc67
Jan Timpe <jtimpe@goraft.tech>
2023-07-10 13:39:05 -0400

    update settings

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -92,9 +92,12 @@ class Common(Configuration):
     FRONTEND_BASE_URL = os.getenv('FRONTEND_BASE_URL', 'http://localhost:3000')
 
     # Email Server
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.ees.hhs.gov"
-    EMAIL_HOST_USER = "no-reply@tanfdata.acf.hhs.gov"
+    # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    # EMAIL_HOST = "smtp.ees.hhs.gov"
+    # EMAIL_HOST_USER = "no-reply@tanfdata.acf.hhs.gov"
+    EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+    SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY', None)
+
 
     # Whether to use localstack in place of a live AWS S3 environment
     USE_LOCALSTACK = bool(strtobool(os.getenv("USE_LOCALSTACK", "no")))
