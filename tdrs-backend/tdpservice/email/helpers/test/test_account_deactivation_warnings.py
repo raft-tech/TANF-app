bcc19e2ce371af5d36aaa426d8e27c24c7e35aec
Jan Timpe <jtimpe@goraft.tech>
2023-07-19 13:24:54 -0400

    update tests to be synchronous

--- tdrs-backend/tdpservice/email/helpers/test/test_account_deactivation_warnings.py
+++ tdrs-backend/tdpservice/email/helpers/test/test_account_deactivation_warnings.py
@@ -1,6 +1,6 @@
 """Test functions for email helper."""
 import pytest
-
+from django.core import mail
 import tdpservice
 from tdpservice.email.helpers.account_deactivation_warning import send_deactivation_warning_email
 
@@ -8,13 +8,8 @@ from tdpservice.email.helpers.account_deactivation_warning import send_deactivat
 @pytest.mark.django_db
 def test_send_deactivation_warning_email(mocker, user):
     """Test that the send_deactivation_warning_email function runs."""
-    mocker.patch(
-        'tdpservice.email.helpers.account_deactivation_warning.automated_email.delay',
-        return_value=None
-    )
     send_deactivation_warning_email([user], 10)
 
-    assert tdpservice.email.email.automated_email.delay.called_once_with(
-        recipient_email=user.email,
-        subject='Account Deactivation Warning: 10 days remaining',
-    )
+    assert len(mail.outbox) == 1
+    assert mail.outbox[0].subject == "Account Deactivation Warning: 10 days remaining"
+
