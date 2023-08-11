ccafb33d98cf4dfd478bfb7123aa7972f4d31159
mo sohani <msohani@goraft.tech>
2023-08-11 09:03:25 -0400

    separating the file_upload

--- tdrs-frontend/cypress/e2e/accounts/accounts.js
+++ tdrs-frontend/cypress/e2e/accounts/accounts.js
@@ -35,6 +35,29 @@ When('{string} is in begin state', (username) => {
   })
 })
 
+When('{string} is in approved state', (username) => {
+  cy.get('@cypressUser').then((cypressUser) => {
+    let body = {
+      username: username,
+      first_name: '',
+      last_name: '',
+      email: username,
+      stt: '6',
+      region: '',
+      groups: '2',
+      account_approval_status: 'Approved',
+      access_requested_date_0: '0001-01-01',
+      access_requested_date_1: '00:00:00',
+      _save: 'Save',
+    }
+    cy.adminApiRequest(
+      'POST',
+      `/users/user/${cypressUser.selector.id}/change/`,
+      body
+    )
+  })
+})
+
 When('{string} requests access', (username) => {
   cy.get('#firstName').type('cypress')
   cy.get('#lastName').type('cypress')
@@ -56,33 +79,4 @@ Then('{string} cannot log in', (username) => {
 Then('{string} sees the request still submitted', (username) => {
   cy.visit('/')
   cy.contains('Request Submitted').should('exist')
-})
-
-Then('{string} can see Data Files page', (username) => {
-  cy.visit('/data-files')
-  cy.contains('Data Files').should('exist')
-})
-
-Then('{string} can see search form', (username) => {
-  cy.contains('Fiscal Year').should('exist')
-  cy.contains('Quarter').should('exist')
-})
-
-Then('{string} can browse upload file form', (username) => {
-  cy.get('#reportingYears').should('exist').select('2023')
-  cy.get('#quarter').should('exist').select('Q1')
-  cy.get('button').contains('Search').should('exist')
-})
-
-When('{string} uploads a file', (username) => {
-  cy.get('button').contains('Search').should('exist').click()
-  cy.get('#closed-case-data').selectFile('../tdrs-backend/tdpservice/parsers/test/data/small_correct_file',{ action: 'drag-drop' })
-  cy.get('button').contains('Submit Data Files').should('exist').click()
-
-})
-
-Then('{string} can see the upload successful', (username) => {
-  cy.wait(3000).then(() => {
-    cy.contains(/Successfully|No changes/g).should('exist')
-  })
 })
