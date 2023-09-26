faffedf28186ab0b6ce9d7ed6f30ea5446d5db55
Jan Timpe <jtimpe@goraft.tech>
2023-09-26 12:40:45 -0400

    rm

--- commands.sh
+++ commands.sh
@@ -235,5 +235,3 @@ alias tdrs-functions='declare -F|grep tdrs && alias|grep tdrs|cut -d" " -f1 --co
 
 # Get logs on backend
 alias tdrs-backend-log="docker logs $(docker ps|grep web|awk '{print $1}')"
-
-##
