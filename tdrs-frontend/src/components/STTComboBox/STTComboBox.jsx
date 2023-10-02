f8c6131ea663ac987dd79f3bd61639d2f94d0939
Eric Lipe <elipe@teamraft.com>
2023-10-02 12:12:02 -0600

    - Adding error modal when stt list is empty

--- tdrs-frontend/src/components/STTComboBox/STTComboBox.jsx
+++ tdrs-frontend/src/components/STTComboBox/STTComboBox.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchSttList } from '../../actions/sttList'
+import { fetchSttList, EMPTY_STTS_LIST_ERROR } from '../../actions/sttList'
 import ComboBox from '../ComboBox'
+import Button from '../Button'
 
 /**
  * @param {function} selectStt - Function to reference and change the
@@ -17,35 +18,70 @@ function STTComboBox({ selectStt, selectedStt, handleBlur, error }) {
   const sttList = useSelector((state) => state?.stts?.sttList)
   const dispatch = useDispatch()
   const [numTries, setNumTries] = useState(0)
+  const [reachedMaxTries, setReachedMaxTries] = useState(false)
 
   useEffect(() => {
     if (sttList.length === 0 && numTries < 6) {
       dispatch(fetchSttList())
-      console.log('Num Tries: %d', numTries)
       setNumTries(numTries + 1)
+    } else {
+      setReachedMaxTries(true)
     }
   }, [dispatch, sttList, numTries])
 
+  const modalRef = useRef()
+  const headerRef = useRef()
+  const onSignOut = () => {
+    window.location.href = `${process.env.REACT_APP_BACKEND_URL}/logout/oidc`
+  }
+
   return (
-    <ComboBox
-      name="stt"
-      label="Associated State, Tribe, or Territory*"
-      error={error ? 'A state, tribe, or territory is required' : undefined}
-      handleSelect={selectStt}
-      selected={selectedStt}
-      handleBlur={handleBlur}
-      placeholder="- Select or Search -"
-      aria-required="true"
-    >
-      <option value="" disabled hidden>
-        - Select or Search -
-      </option>
-      {sttList.map((stt) => (
-        <option className="sttOption" key={stt.id} value={stt.name}>
-          {stt.name}
+    <>
+      <ComboBox
+        name="stt"
+        label="Associated State, Tribe, or Territory*"
+        error={error ? 'A state, tribe, or territory is required' : undefined}
+        handleSelect={selectStt}
+        selected={selectedStt}
+        handleBlur={handleBlur}
+        placeholder="- Select or Search -"
+        aria-required="true"
+      >
+        <option value="" disabled hidden>
+          - Select or Search -
         </option>
-      ))}
-    </ComboBox>
+        {sttList.map((stt) => (
+          <option className="sttOption" key={stt.id} value={stt.name}>
+            {stt.name}
+          </option>
+        ))}
+      </ComboBox>
+      <div
+        id="emptySttListModal"
+        className={`modal ${
+          reachedMaxTries ? 'display-block' : 'display-none'
+        }`}
+      >
+        <div className="modal-content" ref={modalRef}>
+          <h1
+            className="font-serif-xl margin-4 margin-bottom-0 text-normal"
+            tabIndex="-1"
+            ref={headerRef}
+          >
+            TDP systems are currently experiencing technical difficulties.
+          </h1>
+          <p className="margin-4 margin-top-1">
+            Please sign out and try signing in again. If the issue persists
+            contact support at tanfdata@acf.hhs.gov.
+          </p>
+          <div className="margin-x-4 margin-bottom-4">
+            <Button type="button" className="sign-out" onClick={onSignOut}>
+              Sign Out Now
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
