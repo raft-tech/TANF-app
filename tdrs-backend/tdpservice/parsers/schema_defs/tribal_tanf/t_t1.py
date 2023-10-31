0408123e2ca19371c1924713e3486adc0837fe5f
Eric Lipe <elipe@teamraft.com>
2023-10-31 13:28:17 -0600

    - Update t1 field validators

--- tdrs-backend/tdpservice/parsers/schema_defs/tribal_tanf/t_t1.py
+++ tdrs-backend/tdpservice/parsers/schema_defs/tribal_tanf/t_t1.py
@@ -88,11 +88,11 @@ t_t1 = SchemaManager(schemas=[
                   Field(item="6", name='CASE_NUMBER', type='string', startIndex=8, endIndex=19,
                         required=True, validators=[validators.isAlphaNumeric()]),
                   Field(item="2", name='COUNTY_FIPS_CODE', type='string', startIndex=19, endIndex=22,
-                        required=True, validators=[
+                        required=False, validators=[
                               validators.isNumber(),
                         ]),
                   Field(item="5", name='STRATUM', type='string', startIndex=22, endIndex=24,
-                        required=True, validators=[
+                        required=False, validators=[
                               validators.isInStringRange(0, 99),
                         ]),
                   Field(item="7", name='ZIP_CODE', type='string', startIndex=24, endIndex=29,
@@ -101,11 +101,11 @@ t_t1 = SchemaManager(schemas=[
                         ]),
                   Field(item="8", name='FUNDING_STREAM', type='number', startIndex=29, endIndex=30,
                         required=True, validators=[
-                              validators.isInLimits(1, 3),
+                              validators.isInLimits(1, 2),
                         ]),
                   Field(item="9", name='DISPOSITION', type='number', startIndex=30, endIndex=31,
                         required=True, validators=[
-                              validators.oneOf([1, 2]),
+                              validators.oneOf([1]),
                         ]),
                   Field(item="10", name='NEW_APPLICANT', type='number', startIndex=31, endIndex=32,
                         required=True, validators=[
@@ -113,7 +113,7 @@ t_t1 = SchemaManager(schemas=[
                         ]),
                   Field(item="11", name='NBR_FAMILY_MEMBERS', type='number', startIndex=32, endIndex=34,
                         required=True, validators=[
-                              validators.isLargerThan(0),
+                              validators.isInLimits(1, 99),
                         ]),
                   Field(item="12", name='FAMILY_TYPE', type='number', startIndex=34, endIndex=35,
                         required=True, validators=[
@@ -128,83 +128,83 @@ t_t1 = SchemaManager(schemas=[
                               validators.isInLimits(1, 2),
                         ]),
                   Field(item="15", name='RECEIVES_FOOD_STAMPS', type='number', startIndex=37, endIndex=38,
-                        required=True, validators=[
+                        required=False, validators=[
                               validators.isInLimits(0, 2),
                         ]),
                   Field(item="16", name='AMT_FOOD_STAMP_ASSISTANCE', type='number', startIndex=38, endIndex=42,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="17", name='RECEIVES_SUB_CC', type='number', startIndex=42, endIndex=43,
-                        required=True, validators=[
+                        required=False, validators=[
                               validators.isInLimits(0, 3),
                         ]),
                   Field(item="18", name='AMT_SUB_CC', type='number', startIndex=43, endIndex=47,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="19", name='CHILD_SUPPORT_AMT', type='number', startIndex=47, endIndex=51,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="20", name='FAMILY_CASH_RESOURCES', type='number', startIndex=51, endIndex=55,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="21A", name='CASH_AMOUNT', type='number', startIndex=55, endIndex=59,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="21B", name='NBR_MONTHS', type='number', startIndex=59, endIndex=62,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 999),
                         ]),
                   Field(item="22A", name='CC_AMOUNT', type='number', startIndex=62, endIndex=66,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="22B", name='CHILDREN_COVERED', type='number', startIndex=66, endIndex=68,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 99),
                         ]),
                   Field(item="22C", name='CC_NBR_MONTHS', type='number', startIndex=68, endIndex=71,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 999),
                         ]),
                   Field(item="23A", name='TRANSP_AMOUNT', type='number', startIndex=71, endIndex=75,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="23B", name='TRANSP_NBR_MONTHS', type='number', startIndex=75, endIndex=78,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 999),
                         ]),
                   Field(item="24A", name='TRANSITION_SERVICES_AMOUNT', type='number', startIndex=78, endIndex=82,
-                        required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                        required=False, validators=[
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="24B", name='TRANSITION_NBR_MONTHS', type='number', startIndex=82, endIndex=85,
-                        required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                        required=False, validators=[
+                              validators.isInLimits(0, 999),
                         ]),
                   Field(item="25A", name='OTHER_AMOUNT', type='number', startIndex=85, endIndex=89,
-                        required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                        required=False, validators=[
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="25B", name='OTHER_NBR_MONTHS', type='number', startIndex=89, endIndex=92,
-                        required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                        required=False, validators=[
+                              validators.isInLimits(0, 999),
                         ]),
                   Field(item="26AI", name='SANC_REDUCTION_AMT', type='number', startIndex=92, endIndex=96,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="26AII", name='WORK_REQ_SANCTION', type='number', startIndex=96, endIndex=97,
                         required=True, validators=[
                               validators.oneOf([1, 2]),
                         ]),
                   Field(item="26AIII", name='FAMILY_SANC_ADULT', type='number', startIndex=97, endIndex=98,
-                        required=True, validators=[
+                        required=False, validators=[
                               validators.oneOf([0, 1, 2]),
                         ]),
                   Field(item="26AIV", name='SANC_TEEN_PARENT', type='number', startIndex=98, endIndex=99,
@@ -225,11 +225,11 @@ t_t1 = SchemaManager(schemas=[
                         ]),
                   Field(item="26B", name='RECOUPMENT_PRIOR_OVRPMT', type='number', startIndex=102, endIndex=106,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="26CI", name='OTHER_TOTAL_REDUCTIONS', type='number', startIndex=106, endIndex=110,
                         required=True, validators=[
-                              validators.isLargerThanOrEqualTo(0),
+                              validators.isInLimits(0, 9999),
                         ]),
                   Field(item="26CII", name='FAMILY_CAP', type='number', startIndex=110, endIndex=111,
                         required=True, validators=[
@@ -244,18 +244,12 @@ t_t1 = SchemaManager(schemas=[
                               validators.oneOf([1, 2]),
                         ]),
                   Field(item="27", name='WAIVER_EVAL_CONTROL_GRPS', type='string', startIndex=113, endIndex=114,
-                        required=True, validators=[
-                              validators.or_validators(validators.matches('9'), validators.isEmpty()),
-                              validators.isAlphaNumeric(),
-                        ]),
+                        required=False, validators=[validators.isInStringRange(0, 9)]),
                   Field(item="28", name='FAMILY_EXEMPT_TIME_LIMITS', type='number', startIndex=114, endIndex=116,
-                        required=True, validators=[
-                              validators.oneOf([1, 2, 3, 4,
-                                                6, 7, 8, 9])
-                        ]),
+                        required=True, validators=[validators.isInLimits(0, 9)]),
                   Field(item="29", name='FAMILY_NEW_CHILD', type='number', startIndex=116, endIndex=117,
-                        required=True, validators=[
-                              validators.oneOf([1, 2]),
+                        required=False, validators=[
+                              validators.oneOf([0, 1, 2]),
                         ]),
                   Field(item="-1", name='BLANK', type='string', startIndex=117, endIndex=123, required=False,
                         validators=[]),
