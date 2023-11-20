9154b303fe7afe0827d65beef078bf14df1fc2f8
Eric Lipe <elipe@teamraft.com>
2023-11-20 15:20:51 -0700

    - Adding missing postparsing validator

--- tdrs-backend/tdpservice/parsers/schema_defs/tanf/t7.py
+++ tdrs-backend/tdpservice/parsers/schema_defs/tanf/t7.py
@@ -23,6 +23,7 @@ for i in range(1, 31):
             model=TANF_T7,
             quiet_preparser_errors=i > 1,
             preparsing_validators=[
+                validators.hasLength(247),
                 validators.notEmpty(0, 7),
                 validators.notEmpty(validator_index, validator_index + 24),
             ],
