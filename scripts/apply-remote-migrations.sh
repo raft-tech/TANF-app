1d6b756e72c1e7decda5c60f417867f7734e5db2
Jan Timpe <jtimpe@goraft.tech>
2023-11-24 12:06:42 -0500

    comment critical steps - leave ssh tunnel open

--- scripts/apply-remote-migrations.sh
+++ scripts/apply-remote-migrations.sh
@@ -9,7 +9,7 @@ echo $guid
 creds=$(cf curl /v2/apps/$guid/env | jq -r '.system_env_json.VCAP_SERVICES."aws-rds"[0].credentials')
 connection_str=$(echo $creds | jq -r '[.host, .port]' | jq -r 'join(":")')
 
-# echo $connection_str
+echo $connection_str
 # echo $creds
 
 echo "Starting tunnel..."
@@ -26,14 +26,15 @@ echo "DB_PASSWORD=$(echo $creds | jq -r '.password')" >> ./tdrs-backend/.env.ci
 
 
 echo "Starting application..."
-docker-compose -f ./tdrs-backend/docker-compose.ci.yml --env-file ./tdrs-backend/.env.ci up -d
+# docker-compose -f ./tdrs-backend/docker-compose.ci.yml --env-file ./tdrs-backend/.env.ci up -d
+# some sort of `docker run` would likely be preferable here
 echo "Done."
 
 echo "Applying migrations..."
-docker-compose -f ./tdrs-backend/docker-compose.ci.yml exec web python manage.py migrate
+# docker-compose -f ./tdrs-backend/docker-compose.ci.yml exec web python manage.py migrate
 echo "Done."
 
 echo "Cleaning up..."
-docker-compose -f ./tdrs-backend/docker-compose.ci.yml down -v
+# docker-compose -f ./tdrs-backend/docker-compose.ci.yml down -v
 # kill script pid (?)
 echo "Done."
