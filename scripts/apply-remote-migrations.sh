7d407224324ee64d0ddaa863c6a3c65dc353a0fc
Jan Timpe <jtimpe@goraft.tech>
2023-11-27 13:10:26 -0500

    go back to docker compose setup

--- scripts/apply-remote-migrations.sh
+++ scripts/apply-remote-migrations.sh
@@ -16,11 +16,12 @@ echo $connection_str
 
 echo "Starting tunnel..."
 cf ssh -N -L 5432:$connection_str $app &
+sleep 5
 echo "Done."
 
 cp ./.env.example ./.env.ci
 
-echo "DB_NAME=$(echo $creds | jq -r '.name')" >> ./.env.ci # usually needs to be a different value
+echo "DB_NAME=$(echo $creds | jq -r '.name')" >> ./.env.ci # usually needs to be a different valuec
 echo "DB_HOST=host.docker.internal" >> ./.env.ci
 echo "DB_PORT=$(echo $creds | jq -r '.port')" >> ./.env.ci
 echo "DB_USER=$(echo $creds | jq -r '.username')" >> ./.env.ci
@@ -29,21 +30,23 @@ echo "DB_PASSWORD=$(echo $creds | jq -r '.password')" >> ./.env.ci
 
 echo "Starting container..."
 # docker-compose -f ./tdrs-backend/docker-compose.ci.yml --env-file ./tdrs-backend/.env.ci up -d
+docker-compose -f docker-compose.ci.yml --env-file .env.ci up -d
 # some sort of `docker run` would likely be preferable here
-docker build -t web:ci .
-docker run -itd --name web --add-host=host.docker.internal:host-gateway web:ci
+# docker build -t web:ci .
+# docker run -itd --name web --add-host=host.docker.internal:host-gateway web:ci
 echo "Done."
 
 echo "Applying migrations..."
 # docker-compose -f ./tdrs-backend/docker-compose.ci.yml exec web python manage.py migrate
-docker exec --env-file ./.env.ci web python manage.py migrate
+docker-compose -f docker-compose.ci.yml exec web python manage.py migrate
+# docker exec --env-file ./.env.ci web python manage.py migrate
 echo "Done."
 
 echo "Cleaning up..."
-# docker-compose -f ./tdrs-backend/docker-compose.ci.yml down -v
-docker stop web
-docker rm web
+docker-compose -f docker-compose.ci.yml down -v
+# docker stop web
+# docker rm web
 kill $!
-rm ./.env.ci
+# rm ./.env.ci
 cd ..
 echo "Done."
