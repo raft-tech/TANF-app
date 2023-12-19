6495c78383424c6ab4479908bdb18c3a4fe16c22
George Hudson <ghudson@teamraft.com>
2023-12-19 13:49:22 -0700

    added debug info

--- tdrs-backend/celery_start.sh
+++ tdrs-backend/celery_start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+echo starting celery
 celery -A tdpservice.settings worker -c 1 &
 sleep 5
 # TODO: Uncomment the following line to add flower service when memory limitation is resolved
+echo "REDIS_URI: $REDIS_URI"
 celery -A tdpservice.settings --broker=$REDIS_URI flower &
 celery -A tdpservice.settings beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
