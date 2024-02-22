0ee3a7de59543fd633b6d94ddb84703a46a254b2
andrew-jameson <ajameson@teamraft.com>
2024-02-22 15:06:25 -0500

    fake bad migrations, hopefully

--- tdrs-backend/tdpservice/stts/models.py
+++ tdrs-backend/tdpservice/stts/models.py
@@ -37,7 +37,7 @@ class STT(models.Model):
     state = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True)
     ssp = models.BooleanField(default=False, null=True)
     sample = models.BooleanField(default=False, null=True)
-    fake_field = models.ForeignKey(Region, on_delete=models.CASCADE, blank=True, null=True)
+    fa_feld = models.ForeignKey(Region, on_delete=models.CASCADE, blank=True, null=True)
 
     class Meta:
         """Metadata."""
