ee4130639ba543b54a818c541138cab519cf1b1f
Eric Lipe <elipe@teamraft.com>
2024-05-28 13:50:35 -0400

    Revert "- Move run-task command into zap script"
    This reverts commit f1f7910bdc42ab3e687d13599717aa9a92be97a8.

--- scripts/zap-scanner.sh
+++ scripts/zap-scanner.sh
@@ -200,27 +200,3 @@ if [ "$ENVIRONMENT" = "nightly" ]; then
     echo "export ZAP_${TARGET}_FAIL_COUNT=$ZAP_FAIL_COUNT"
   } >> "$BASH_ENV"
 fi
-
-
-# Construct the project slug from the current branch name and user
-PROJECT_SLUG=$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME
-# These environment variables are exported to Circle CI's BASH_ENV
-# by the zap-scanner.sh script for each respective app target.
-CMD_ARGS=(
-  "$CIRCLE_BUILD_NUM"
-  --backend-pass-count "${ZAP_BACKEND_PASS_COUNT:-0}"
-  --backend-warn-count "${ZAP_BACKEND_WARN_COUNT:-0}"
-  --backend-fail-count "${ZAP_BACKEND_FAIL_COUNT:-0}"
-  --frontend-pass-count "${ZAP_FRONTEND_PASS_COUNT:-0}"
-  --frontend-warn-count "${ZAP_FRONTEND_WARN_COUNT:-0}"
-  --frontend-fail-count "${ZAP_FRONTEND_FAIL_COUNT:-0}"
-  --project-slug "$PROJECT_SLUG"
-)
-# Evaluate the full command before passing it in so it doesn't
-# get improperly interpolated by Cloud.gov.
-CMD="python manage.py process_owasp_scan ${CMD_ARGS[*]}"
-# Submit a CF Task for execution after a 4 minute sleep to ensure all of the scan's previous state has been closed.
-# sleep 240
-cf run-task tdp-backend-$TARGET_ENV \
-  --command "$CMD" \
-  --name nightly-owasp-scan
