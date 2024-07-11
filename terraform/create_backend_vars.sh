6fadcb0ac2245a2bed0bc6dbf564435d4cc518a8
Eric Lipe <elipe@teamraft.com>
2024-07-11 13:58:19 -0400

    - Made scripts a little smarter

--- terraform/create_backend_vars.sh
+++ terraform/create_backend_vars.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+if [[ $# -eq 0 ]] ; then
+    echo 'You need to pass the env you are configuring: 'dev', 'staging', 'production'.'
+    exit 1
+fi
+
+if [[ "$1" != "dev" && "$1" != "staging" && "$1" != "production" ]] ; then
+  echo 'The first argument to this script must be one of: 'dev', 'staging', or 'production'.'
+  exit 1
+fi
+
 S3_CREDENTIALS=$(cf service-key tdp-tf-states tdp-tf-key | tail -n +2)
 if [ -z "$S3_CREDENTIALS" ]; then
   echo "Unable to get service-keys, you may need to login to Cloud.gov first"
@@ -18,4 +28,4 @@ BUCKET=$(echo "${S3_CREDENTIALS}" | jq -r '.credentials.bucket')
   echo "secret_key = \"$SECRET_KEY\""
   echo "region = \"$REGION\""
   echo "bucket = \"$BUCKET\""
-} > ./dev/backend_config.tfvars
+} > ./$1/backend_config.tfvars
--- terraform/create_tf_vars.sh
+++ terraform/create_tf_vars.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+if [[ $# -eq 0 ]] ; then
+    echo 'You need to pass the env you are configuring: 'dev', 'staging', 'production'.'
+    exit 1
+fi
+
+if [[ "$1" != "dev" && "$1" != "staging" && "$1" != "production" ]] ; then
+  echo 'The first argument to this script must be one of: 'dev', 'staging', or 'production'.'
+  exit 1
+fi
+
 KEYS_JSON=$(cf service-key tanf-keys deployer | grep -A4 "{")
 if [ -z "$KEYS_JSON" ]; then
   echo "Unable to get service-keys, you may need to login to Cloud.gov first"
@@ -17,4 +27,4 @@ CF_SPACE="tanf-dev"
   echo "cf_password = \"$CF_PASSWORD_DEV\""
   echo "cf_user = \"$CF_USERNAME_DEV\""
   echo "cf_space_name = \"$CF_SPACE\""
-} > ./dev/variables.tfvars
+} > ./$1/variables.tfvars
