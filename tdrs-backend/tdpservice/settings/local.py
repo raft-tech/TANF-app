63f73c705ba4ae15eaafe243595a9e6b073245d6
Mo Sohani <msohani@goraft.tech>
2024-08-01 10:50:59 -0400

    updated backup file and script

--- tdrs-backend/tdpservice/settings/local.py
+++ tdrs-backend/tdpservice/settings/local.py
@@ -52,7 +52,7 @@ class Local(Common):
 
     # SENTRY
     sentry_sdk.init(
-        dsn="http://99f21b99fdc8ad374857c9cd8e45f34f@host.docker.internal:9001/2",
+        dsn="http://43ebf8abe1434ec6aea2c7b92c465a0e@host.docker.internal:9001/2",
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         integrations=[
