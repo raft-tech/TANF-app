fdbfd29b05b65de017f431d11d5d72b449a98fa2
Eric Lipe <elipe@teamraft.com>
2024-08-05 13:51:33 -0400

    - Undo debug adds

--- tdrs-backend/test.py
+++
@@ -1,9 +0,0 @@
-import statistics
-l = []
-with open("/Users/ericlipe/work/repos/tdrs/TANF-app/tdrs-backend/res.txt", 'r') as f:
-    for line in f:
-        l.append(float(line))
-print(len(l))
-print(sum(l))
-print(sum(l) / len(l))
-print(statistics.median(l))
