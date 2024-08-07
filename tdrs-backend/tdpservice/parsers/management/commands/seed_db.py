6d23208c87f0df509ff094836b0652e22eb2f21f
andrew-jameson <ajameson@teamraft.com>
2024-08-07 13:12:27 -0400

    Pushing latest prior to lunch

--- tdrs-backend/tdpservice/parsers/management/commands/seed_db.py
+++ tdrs-backend/tdpservice/parsers/management/commands/seed_db.py
@@ -1,16 +1,19 @@
 """`seed_db` command."""
 
-import json
+import faker
+import random
 import logging
 from pathlib import Path
 
 from django.core.management import BaseCommand
 from django.utils import timezone
 
-from tdpservice.parsers.schema_defs.utils import get_schema_options, get_text_from_df # maybe need other utilities
+
 from tdpservice.parsers.test.factories import ParsingFileFactory, TanfT1Factory # maybe need other factories
 from tdpservice.parsers.schema_defs.header import header
-from tdpservice.parsers.schema_defs.tanf import t1, t2, t3, t4, t5, t6, t7
+from tdpservice.parsers.schema_defs.trailer import trailer
+from tdpservice.parsers.schema_defs.utils import *  # maybe need other utilities
+# all models should be referenced by using the utils.py get_schema_options wrappers
 from tdpservice.parsers import schema_defs
 from tdpservice.data_files.models import DataFile
 logger = logging.getLogger(__name__)
@@ -38,6 +41,7 @@ Tool will "fuzz" or generate out of range values to intentionally create issues 
 Create django command such that tool can be pointed at deployed environments 
 '''
 
+'''
 schema_options = {
     'TAN': {
         'A': {
@@ -127,6 +131,186 @@ schema_options = {
         },
     },
 }
+'''
+
+# t1 fields
+'''
+RecordType(0-2)
+RPT_MONTH_YEAR(2-8)
+CASE_NUMBER(8-19)
+COUNTY_FIPS_CODE(19-22)
+STRATUM(22-24)
+ZIP_CODE(24-29)
+FUNDING_STREAM(29-30)
+DISPOSITION(30-31)
+NEW_APPLICANT(31-32)
+NBR_FAMILY_MEMBERS(32-34)
+FAMILY_TYPE(34-35)
+RECEIVES_SUB_HOUSING(35-36)
+RECEIVES_MED_ASSISTANCE(36-37)
+RECEIVES_FOOD_STAMPS(37-38)
+AMT_FOOD_STAMP_ASSISTANCE(38-42)
+RECEIVES_SUB_CC(42-43)
+AMT_SUB_CC(43-47)
+CHILD_SUPPORT_AMT(47-51)
+FAMILY_CASH_RESOURCES(51-55)
+CASH_AMOUNT(55-59)
+NBR_MONTHS(59-62)
+CC_AMOUNT(62-66)
+CHILDREN_COVERED(66-68)
+CC_NBR_MONTHS(68-71)
+TRANSP_AMOUNT(71-75)
+TRANSP_NBR_MONTHS(75-78)
+TRANSITION_SERVICES_AMOUNT(78-82)
+TRANSITION_NBR_MONTHS(82-85)
+OTHER_AMOUNT(85-89)
+OTHER_NBR_MONTHS(89-92)
+SANC_REDUCTION_AMT(92-96)
+WORK_REQ_SANCTION(96-97)
+FAMILY_SANC_ADULT(97-98)
+SANC_TEEN_PARENT(98-99)
+NON_COOPERATION_CSE(99-100)
+FAILURE_TO_COMPLY(100-101)
+OTHER_SANCTION(101-102)
+RECOUPMENT_PRIOR_OVRPMT(102-106)
+OTHER_TOTAL_REDUCTIONS(106-110)
+FAMILY_CAP(110-111)
+REDUCTIONS_ON_RECEIPTS(111-112)
+OTHER_NON_SANCTION(112-113)
+WAIVER_EVAL_CONTROL_GRPS(113-114)
+FAMILY_EXEMPT_TIME_LIMITS(114-116)
+FAMILY_NEW_CHILD(116-117)
+BLANK(117-156)
+'''
+# https://faker.readthedocs.io/en/stable/providers/baseprovider.html#faker.providers.BaseProvider
+class FieldFaker(faker.providers.BaseProvider):
+    def record_type(self):
+        return self.random_element(elements=('00', '01', '02'))
+
+    def rpt_month_year(self):
+        return self.date_time_this_month(before_now=True, after_now=False).strftime('%m%Y')
+
+    def case_number(self):
+        return self.random_int(min=1000000000, max=9999999999)
+
+    def county_fips_code(self):
+        return self.random_int(min=0, max=999)
+
+    def stratum(self):
+        return self.random_int(min=0, max=99)
+
+    def zip_code(self):
+        return self.random_int(min=0, max=99999)
+
+    def funding_stream(self):
+        return self.random_element(elements=('A', 'B', 'C'))
+
+    def disposition(self):
+        return self.random_element(elements=('A', 'B', 'C'))
+
+    def new_applicant(self):
+        return self.random_element(elements=('Y', 'N'))
+
+    def nbr_family_members(self):
+        return self.random_int(min=0, max=99)
+
+    def family_type(self):
+        return self.random_element(elements=('A', 'B', 'C'))
+
+    def receives_sub_housing(self):
+        return self.random_element(elements=('Y', 'N'))
+
+def build_datafile(year, quarter, original_filename, file_name, section, file_data):
+    """Build a datafile."""
+    return ParsingFileFactory.build(
+        year=year,
+        quarter=quarter,
+        original_filename=original_filename,
+        file__name=file_name,
+        section=section,
+        file__data=file_data,
+    )
+
+def validValues(field):
+    '''Takes in a field and returns a line of valid values.'''
+    #niave implementation will just zero or 'a' fill the field
+    '''field_len = field.endIndex - field.startIndex
+    if field.type is "number":
+        line += "0" * field_len
+    elif field.type is "string":
+        line += "A" * field_len
+    else:
+        raise ValueError("Field type not recognized")
+    return line'''
+    #brute implementation will use faker and we'll run it through the validators
+    #pass
+
+    # elegant implementation will use the validators to generate the valid values
+
+    field_len = field.endIndex - field.startIndex
+    # check list of validators
+        # treat header/trailer special, it actually checks for string values
+        # check for zero or pad fill
+        # transformField might be tricky
+    if field.name == 'SSN':
+        # only used by recordtypes 2,3,5 
+        # TODO: reverse the TransformField logic to 'encrypt' a random number
+        field_format = '?' * field_len
+    else:
+        field_format = '#' * field_len
+    return faker.bothify(text=field_format)
+
+
+def make_line(schemaMgr):
+    '''Takes in a schema manager and returns a line of data.'''
+    line = ''
+
+    #TODO: check for header/trailer
+    if schemaMgr.record_type == 'HEADER' or schemaMgr.record_type == 'TRAILER':
+        for field in schemaMgr.fields:
+            line += validValues(field)
+
+    else:
+        for field in schemaMgr.fields:
+            line += validValues(field)
+    return line
+
+def make_files(stt, year, quarter):
+    '''Given a STT, parameterize calls to build_datafile and make_line.'''
+    """Psuedo code"""
+    sections = stt.filenames
+
+    for section in sections:
+
+        # based on section, get models from schema_options
+        
+        models_in_section = get_program_models(stt.program, section)
+        temp_file = ''
+        #TODO: make header line
+        temp_file += make_line(header)
+
+        # iterate over models and generate lines
+        for model in models_in_section:
+            if section in ['Active Case Data', 'Closed Case Data']:
+                # obviously, this first approach can't prevent duplicates (unlikely),
+                #    nor can it ensure that the case data is internally consistent
+                #    (e.g. a case with a child but no adult)
+
+                # we should generate hundreds, thousands, tens of thousands of records
+                for i in range(random.randint(5, 9999)):
+                    temp_file += make_line(model)
+            elif section in ['Aggregate Data', 'Stratum Data']:
+                # we should generate a smaller count of lines...maybe leave this as a TODO
+                # shouldn't this be based on the active/closed case data?
+                pass
+
+        # make trailer line
+        temp_file += make_line(trailer)
+
+        # build datafile
+
+    # return dictionary of binary blobs
+    # return {'Active Case Data': b'...', 'Closed Case Data': b'...', 'Aggregate Data': b'...', 'Stratum Data': b'...'}
 
 class Command(BaseCommand):
     """Command class."""
@@ -144,9 +328,6 @@ class Command(BaseCommand):
             def get_text_from_df(df):
             def get_schema(line, section, program_type): """
     
-       
-    
-
     
         """ parsing_file = ParsingFileFactory.build(year=2021,
             quarter='Q2',
@@ -163,45 +344,43 @@ class Command(BaseCommand):
     
         # t1 = schema_options.get('TAN').get('A').get('models').get('T1')
         T1_fields = t1.schemas[0].fields
+
         [print(i) for i in T1_fields]
 
         t1_line = ''
         for field in T1_fields:
             field_len = field.endIndex - field.startIndex
-            if field.type is "number":
-                t1_line += "0" * field_len
-            elif field.type is "string":
-                t1_line += "a" * field_len
+            # check list of validators
+                # treat header/trailer special, it actually checks for string values
+                # check for zero or pad fill
+                # transformField might be tricky
+            if field.name == 'SSN':
+                # only used by recordtypes 2,3,5 
+                # TODO: reverse the TransformField logic to 'encrypt' a random number
+                field_format = '?' * field_len
             else:
-                raise ValueError("Field type not recognized")
+                field_format = '#' * field_len
+            t1_line += faker.bothify(text=field_format)
         print(t1_line)
 
         # TODO: allowed values per field, try manual and if commonalities exist, create a function to generate
         # TODO: can we utilize validators somehow to get a validValues(schemaMgr.fields[])?
   
-        def validValues():
-            '''Takes in a field and returns a list of valid values.'''
-            #niave implementation will just zero or 'a' fill the field
-            #brute implementation will use faker and we'll run it through the validators
-            # elegant implementation will use the validators to generate the valid values
-            pass
-
-
         '''
         # utilize parsers/schema_defs/utils.py as a reference for getting the lists of STT/years/quarters/sections
         for i in STT[]:
-            for y in years[]
-                for q in quarters[]
-                    for s in sections[]
-                        # now we're generating a binary file? b/c we'll need the DF FK reference
-                        # TODO: 
-                        for r in rowSchemas[]
-                            for f in rowSchemas.Fields[]
-                                for v in f.validValues[]
-                                (look at seed_records.py for how to generate random data)
-                        # write a temp file
-                        # ok now parse this thing (but turn off emails)
-                        # generate a DFS
+            for y in years[] # 1998 - 2099
+                for q in quarters[] # 1-4
+                    for p in programs[] # TAN, SSP, Tribal TAN
+                        for s in sections[] # [x for x['section'] in schema_options[p].keys()]
+                            #need the DF FK reference?
+                            for m in models[]
+                                for f in rowSchemas.Fields[]
+                                    for v in f.validValues[]
+                                    (look at seed_records.py for how to generate random data)
+                            # write a temp file
+                            # ok now parse this thing (but turn off emails)
+                            # generate a DFS
         # dump db in full to a seed file
         '''
         
