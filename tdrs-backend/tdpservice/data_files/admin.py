29dda755852203087dfed402d2ed65573eafb23a
Eric Lipe <elipe@teamraft.com>
2024-08-08 12:47:21 -0400

    - Add admin resources for the many to many relationship - Add helper function to update datafile state during parsing

--- tdrs-backend/tdpservice/data_files/admin.py
+++ tdrs-backend/tdpservice/data_files/admin.py
@@ -30,6 +30,19 @@ class DataFileSummaryPrgTypeFilter(admin.SimpleListFilter):
         else:
             return queryset
 
+
+class DataFileInline(admin.TabularInline):
+    """Inline model for many to many relationship."""
+
+    model = DataFile.reparse_meta_models.through
+    can_delete = False
+    ordering = ["-pk"]
+
+    def has_change_permission(self, request, obj=None):
+        """Read only permissions."""
+        return False
+
+
 @admin.register(DataFile)
 class DataFileAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     """Admin class for DataFile models."""
@@ -54,12 +67,28 @@ class DataFileAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
 
     error_report_link.allow_tags = True
 
+    def rpm_links(self, obj):
+        """Generate a list of links to ReparseMeta models this file is associated with."""
+        rpm_list = "<ul>"
+        # Only show the 5 most recent reparse events the file has been associated with.
+        latest_meta = obj.reparse_meta_models.all().order_by('-id')[:5]
+        if len(latest_meta) > 0:
+            for rpm in latest_meta:
+                url = f'{DOMAIN}/admin/search_indexes/reparsemeta/?id=' + str(rpm.id)
+                rpm_list += f"<li><a href={url}>RPM {rpm.id}</a></li>"
+        else:
+            rpm_list += "<li>None</li>"
+        rpm_list += "</ul>"
+        return format_html(rpm_list)
+
     def data_file_summary(self, obj):
         """Return the data file summary."""
         df = DataFileSummary.objects.get(datafile=obj)
         return format_html("<a href='{url}'>{field}</a>",
                            field=f'{df.id}' + ":" + df.get_status(),
                            url=f"{DOMAIN}/admin/parsers/datafilesummary/{df.id}/change/")
+
+    inlines = [DataFileInline]
 
     list_display = [
         'id',
@@ -70,6 +99,7 @@ class DataFileAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
         'version',
         'data_file_summary',
         'error_report_link',
+        'rpm_links'
     ]
 
     list_filter = [
