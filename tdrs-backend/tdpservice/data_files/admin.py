16bfbceb4c55e8c45160d482b9f4a2f6559babcd
Mo Sohani <msohani@goraft.tech>
2024-08-28 04:02:26 -0400

    3076 Fixed today filter

--- tdrs-backend/tdpservice/data_files/admin.py
+++ tdrs-backend/tdpservice/data_files/admin.py
@@ -62,7 +62,7 @@ class DataFileAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
                            field=f'{df.id}' + ":" + df.get_status(),
                            url=f"{DOMAIN}/admin/parsers/datafilesummary/{df.id}/change/")
 
-    class by_submission_date(admin.SimpleListFilter):
+    class SubmissionDateFilter(admin.SimpleListFilter):
         """filter data files by month."""
 
         title = 'Submission Date'
@@ -83,16 +83,19 @@ class DataFileAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
             yesterday = (datetime.now(tz=timezone.utc) - timedelta(days=1)).replace(
                 hour=0, minute=0, second=0, microsecond=0
                 )
-            this_month = datetime.now(tz=timezone.utc).replace(day=1)
-            this_year = datetime.now(tz=timezone.utc).replace(month=1, day=1)
+            this_month = datetime.now(tz=timezone.utc).replace(
+                day=1, hour=0, minute=0, second=0, microsecond=0)
+            this_year = datetime.now(tz=timezone.utc).replace(
+                month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+            last_week_today = datetime.now(tz=timezone.utc) - timedelta(days=int(self.value()))
+            last_week_today = last_week_today.replace(hour=0, minute=0, second=0, microsecond=0)
             if self.value() == '1':
                 query_set_ids = [df.id for df in queryset if df.created_at.replace(
                     hour=0, minute=0, second=0, microsecond=0
                     ) == yesterday]
                 return queryset.filter(id__in=query_set_ids)
             elif self.value() in ['0', '7']:
-                last_week = datetime.now(tz=timezone.utc) - timedelta(days=int(self.value()))
-                query_set_ids = [df.id for df in queryset if df.created_at >= last_week]
+                query_set_ids = [df.id for df in queryset if df.created_at >= last_week_today]
                 return queryset.filter(id__in=query_set_ids)
             elif self.value() == '30':
                 query_set_ids = [df.id for df in queryset if df.created_at >= this_month]
@@ -120,7 +123,7 @@ class DataFileAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
         'stt',
         'user',
         'year',
-        by_submission_date,
+        SubmissionDateFilter,
         'version',
         'summary__status',
         DataFileSummaryPrgTypeFilter
