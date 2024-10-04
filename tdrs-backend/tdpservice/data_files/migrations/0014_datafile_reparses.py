3be52667b31127f2290d05272aa668542c23acd5
Jan Timpe <jtimpe@goraft.tech>
2024-10-04 09:11:40 -0400

    rearrage migrations

--- tdrs-backend/tdpservice/data_files/migrations/0014_datafile_reparses.py
+++ tdrs-backend/tdpservice/data_files/migrations/0014_datafile_reparses.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data_files', '0014_remove_datafile_reparse_meta_models'),
+        ('data_files', '0014_datafile_reparse_meta'),
         ('search_indexes', '0031_reparsefilemeta'),
     ]
 
