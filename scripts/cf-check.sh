14dbce35cf271098711215153a956b38e50fe99e
andrew-jameson <ajameson@teamraft.com>
2024-10-14 11:28:43 -0400

    cleanup pt2 cf-check

--- scripts/cf-check.sh
+++ scripts/cf-check.sh
@@ -9,7 +9,7 @@ else
     NEXUS_ARCHIVE="cf7-cli_7.7.13_linux_x86-64.tgz"
     NEXUS_URL="https://tdp-nexus.dev.raftlabs.tech/repository/tdp-bin/cloudfoundry-cli/$NEXUS_ARCHIVE"
     curl $NEXUS_URL -o $NEXUS_ARCHIVE  # prefers anonymous, use of -u failed.
-    tar xzf $NEXUS_ARCHIVE #&& rm -f $NEXUS_ARCHIVE
+    tar xzf $NEXUS_ARCHIVE
     mv ./cf /usr/local/bin/
     chmod +x /usr/local/bin/cf
     cf --version
