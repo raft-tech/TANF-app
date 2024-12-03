50b1dda4f116203d9551ca10a0659fa696d5b408
Jan Timpe <jtimpe@goraft.tech>
2024-12-03 12:45:04 -0500

    rm unused

--- tdrs-frontend/src/components/Reports/Reports.test.js
+++ tdrs-frontend/src/components/Reports/Reports.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, waitFor, getByText } from '@testing-library/react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
 
 import { Provider } from 'react-redux'
 import { thunk } from 'redux-thunk'
