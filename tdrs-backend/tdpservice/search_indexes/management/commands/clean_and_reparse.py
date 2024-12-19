be4f37594b2b0bd943cca4cdac6ad6700908adc2
Mo Sohani <msohani@goraft.tech>
2024-12-19 09:20:40 -0500

    linting

--- tdrs-backend/tdpservice/search_indexes/management/commands/clean_and_reparse.py
+++ tdrs-backend/tdpservice/search_indexes/management/commands/clean_and_reparse.py
@@ -1,43 +1,24 @@
 """Delete and reparse a set of datafiles."""
 
 from django.core.management.base import BaseCommand
-from django.core.management import call_command
-from django.core.paginator import Paginator
-from django.db.utils import DatabaseError
-from elasticsearch.exceptions import ElasticsearchException
-from tdpservice.data_files.models import DataFile
-from tdpservice.parsers.models import DataFileSummary, ParserError
-from tdpservice.scheduling import parser_task
-from tdpservice.search_indexes.util import DOCUMENTS, count_all_records
 from tdpservice.search_indexes.models.reparse_meta import ReparseMeta
 from tdpservice.core.utils import log
-from django.contrib.admin.models import ADDITION
 from tdpservice.users.models import User
-from datetime import timedelta
-from django.utils import timezone
-from django.conf import settings
 import logging
-
-logger = logging.getLogger(__name__)
-
 from tdpservice.search_indexes.utils import (
     backup,
     get_log_context,
     assert_sequential_execution,
     should_exit,
     handle_elastic,
-    delete_summaries,
     delete_associated_models,
     count_total_num_records,
     calculate_timeout,
     handle_datafiles,
-    delete_records,
-    delete_errors,
-    handle_elastic_doc_delete,
-    get_files_to_reparse
+    get_files_to_reparse,
 )
 
-
+logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     """Command class."""
 
@@ -45,35 +26,65 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         """Add arguments to the management command."""
-        parser.add_argument("-q", "--fiscal_quarter", type=str, choices=["Q1", "Q2", "Q3", "Q4"],
-                            help="Reparse all files in the fiscal quarter, e.g. Q1.")
-        parser.add_argument("-y", "--fiscal_year", type=int, help="Reparse all files in the fiscal year, e.g. 2021.")
-        parser.add_argument("-a", "--all", action='store_true', help="Clean and reparse all datafiles. If selected, "
-                            "fiscal_year/quarter aren't necessary.")
-        parser.add_argument("-f", "--files", nargs='+', type=str, help="Re-parse specific datafiles by datafile id")
+        parser.add_argument(
+            "-q",
+            "--fiscal_quarter",
+            type=str,
+            choices=["Q1", "Q2", "Q3", "Q4"],
+            help="Reparse all files in the fiscal quarter, e.g. Q1.",
+        )
+        parser.add_argument(
+            "-y",
+            "--fiscal_year",
+            type=int,
+            help="Reparse all files in the fiscal year, e.g. 2021.",
+        )
+        parser.add_argument(
+            "-a",
+            "--all",
+            action="store_true",
+            help="Clean and reparse all datafiles. If selected, "
+            "fiscal_year/quarter aren't necessary.",
+        )
+        parser.add_argument(
+            "-f",
+            "--files",
+            nargs="+",
+            type=str,
+            help="Re-parse specific datafiles by datafile id",
+        )
 
     def _handle_input(self, testing, continue_msg):
         """Handle user input."""
         if not testing:
-            c = str(input(f'\n{continue_msg}\nContinue [y/n]? ')).lower()
-            if c not in ['y', 'yes']:
-                print('Cancelled.')
+            c = str(input(f"\n{continue_msg}\nContinue [y/n]? ")).lower()
+            if c not in ["y", "yes"]:
+                print("Cancelled.")
                 exit(0)
 
     def handle(self, *args, **options):
         """Delete and reparse datafiles matching a query."""
-        fiscal_year = options.get('fiscal_year', None)
-        fiscal_quarter = options.get('fiscal_quarter', None)
-        reparse_all = options.get('all', False)
-        selected_files = options.get('files', None)
-        selected_files = [int(file) for file in selected_files[0].split(',')] if selected_files else None
+        fiscal_year = options.get("fiscal_year", None)
+        fiscal_quarter = options.get("fiscal_quarter", None)
+        reparse_all = options.get("all", False)
+        selected_files = options.get("files", None)
+        selected_files = (
+            [int(file) for file in selected_files[0].split(",")]
+            if selected_files
+            else None
+        )
         new_indices = reparse_all is True
 
         # Option that can only be specified by calling `handle` directly and passing it.
-        testing = options.get('testing', False)
+        testing = options.get("testing", False)
         ##
 
-        args_passed = fiscal_year is not None or fiscal_quarter is not None or reparse_all or selected_files
+        args_passed = (
+            fiscal_year is not None
+            or fiscal_quarter is not None
+            or reparse_all
+            or selected_files
+        )
 
         if not args_passed:
             logger.warning("No arguments supplied.")
@@ -82,55 +93,67 @@ class Command(BaseCommand):
 
         # Set up the backup file name and continue message
         files, backup_file_name, continue_msg = get_files_to_reparse(
-            fiscal_year,
-            fiscal_quarter,
-            selected_files,
-            reparse_all)
+            fiscal_year, fiscal_quarter, selected_files, reparse_all
+        )
 
         # end of the if statement
 
         fmt_str = "be" if new_indices else "NOT be"
-        continue_msg += "will {new_index} stored in new indices and the old indices ".format(new_index=fmt_str)
+        continue_msg += (
+            "will {new_index} stored in new indices and the old indices ".format(
+                new_index=fmt_str
+            )
+        )
 
         num_files = files.count()
         fmt_str = f"ALL ({num_files})" if reparse_all else f"({num_files})"
-        continue_msg += "\nThese options will delete and reparse {0} datafiles.".format(fmt_str)
+        continue_msg += "\nThese options will delete and reparse {0} datafiles.".format(
+            fmt_str
+        )
 
         if not selected_files:
             self._handle_input(testing, continue_msg)
 
-        system_user, created = User.objects.get_or_create(username='system')
+        system_user, created = User.objects.get_or_create(username="system")
         if created:
-            logger.debug('Created reserved system user.')
+            logger.debug("Created reserved system user.")
         log_context = get_log_context(system_user)
 
         all_fy = "All"
         all_q = "Q1-4"
 
         if not selected_files:
-            log(f"Starting clean and reparse command for FY {fiscal_year if fiscal_year else all_fy} and "
+            log(
+                f"Starting clean and reparse command for FY {fiscal_year if fiscal_year else all_fy} and "
                 f"{fiscal_quarter if fiscal_quarter else all_q}",
                 logger_context=log_context,
-                level='info')
+                level="info",
+            )
         else:
-            log(f"Starting clean and reparse action for files: {str(selected_files)}",
+            log(
+                f"Starting clean and reparse action for files: {str(selected_files)}",
                 logger_context=log_context,
-                level='info')
+                level="info",
+            )
 
         if num_files == 0:
-            log(f"No files available for the selected Fiscal Year: {fiscal_year if fiscal_year else all_fy} and "
+            log(
+                f"No files available for the selected Fiscal Year: {fiscal_year if fiscal_year else all_fy} and "
                 f"Quarter: {fiscal_quarter if fiscal_quarter else all_q}. Nothing to do.",
                 logger_context=log_context,
-                level='warn')
+                level="warn",
+            )
             return
 
         is_sequential = assert_sequential_execution(log_context)
         should_exit(not is_sequential)
-        meta_model = ReparseMeta.objects.create(fiscal_quarter=fiscal_quarter,
-                                                fiscal_year=fiscal_year,
-                                                all=reparse_all,
-                                                new_indices=new_indices,
-                                                delete_old_indices=new_indices)
+        meta_model = ReparseMeta.objects.create(
+            fiscal_quarter=fiscal_quarter,
+            fiscal_year=fiscal_year,
+            all=reparse_all,
+            new_indices=new_indices,
+            delete_old_indices=new_indices,
+        )
 
         # Backup the Postgres DB
         backup_file_name += f"_rpv{meta_model.pk}.pg"
@@ -143,26 +166,33 @@ class Command(BaseCommand):
         handle_elastic(new_indices, log_context)
 
         # Delete records from Postgres and Elastic if necessary
-        file_ids = files.values_list('id', flat=True).distinct()
+        file_ids = files.values_list("id", flat=True).distinct()
         meta_model.total_num_records_initial = count_total_num_records(log_context)
         meta_model.save()
 
         delete_associated_models(meta_model, file_ids, new_indices, log_context)
 
-        meta_model.timeout_at = meta_model.created_at + calculate_timeout(num_files,
-                                                                                meta_model.num_records_deleted)
+        meta_model.timeout_at = meta_model.created_at + calculate_timeout(
+            num_files, meta_model.num_records_deleted
+        )
         meta_model.save()
-        logger.info(f"Deleted a total of {meta_model.num_records_deleted} records accross {num_files} files.")
+        logger.info(
+            f"Deleted a total of {meta_model.num_records_deleted} records accross {num_files} files."
+        )
 
         # Delete and re-save datafiles to handle cascading dependencies
-        logger.info(f'Deleting and re-parsing {num_files} files')
+        logger.info(f"Deleting and re-parsing {num_files} files")
         handle_datafiles(files, meta_model, log_context)
 
-        log("Database cleansing complete and all files have been re-scheduling for parsing and validation.",
+        log(
+            "Database cleansing complete and all files have been re-scheduling for parsing and validation.",
             logger_context=log_context,
-            level='info')
-        log(f"Clean and reparse command completed. All files for FY {fiscal_year if fiscal_year else all_fy} and "
+            level="info",
+        )
+        log(
+            f"Clean and reparse command completed. All files for FY {fiscal_year if fiscal_year else all_fy} and "
             f"{fiscal_quarter if fiscal_quarter else all_q} have been queued for parsing.",
             logger_context=log_context,
-            level='info')
-        logger.info('Done. All tasks have been queued to parse the selected datafiles.')
+            level="info",
+        )
+        logger.info("Done. All tasks have been queued to parse the selected datafiles.")
--- tdrs-backend/tdpservice/search_indexes/reparse.py
+++ tdrs-backend/tdpservice/search_indexes/reparse.py
@@ -1,16 +1,9 @@
-# Re-write management command clean_and_reparse as a function without callinf call_command
+"""Reparsing command for selected files."""
 # should include all the steps in the management command
 from tdpservice.data_files.models import DataFile
 from tdpservice.search_indexes.models.reparse_meta import ReparseMeta
 from tdpservice.core.utils import log
-from django.contrib.admin.models import ADDITION
 from tdpservice.users.models import User
-
-import logging
-
-logger = logging.getLogger(__name__)
-
-
 from tdpservice.search_indexes.utils import (
     backup,
     get_log_context,
@@ -22,17 +15,23 @@ from tdpservice.search_indexes.utils import (
     calculate_timeout,
     handle_datafiles,
 )
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def clean_reparse(selected_file_ids):
     """Reparse selected files."""
     selected_files = [int(file_id) for file_id in selected_file_ids[0].split(",")]
-    
+
     ######
     files = DataFile.objects.filter(id__in=selected_files)
     backup_file_name = "/tmp/reparsing_backup"
     backup_file_name += "_selected_files"
     continue_msg = "You have selected to reparse datafiles for FY {fy} and {q}. The reparsed files "
-    continue_msg = continue_msg.format(fy=f"selected files: {str(selected_files)}", q="Q1-4")
+    continue_msg = continue_msg.format(
+        fy=f"selected files: {str(selected_files)}", q="Q1-4"
+    )
 
     num_files = files.count()
 
@@ -46,10 +45,12 @@ def clean_reparse(selected_file_ids):
     all_fy = "All"
     all_q = "Q1-4"
 
-    log(f"Starting clean_and_reparse for {num_files} files",
+    log(
+        f"Starting clean_and_reparse for {num_files} files",
         logger_context=log_context,
-        level=logging.INFO)
-    
+        level=logging.INFO,
+    )
+
     is_sequential = assert_sequential_execution(log_context)
     should_exit(not is_sequential)
 
@@ -58,14 +59,14 @@ def clean_reparse(selected_file_ids):
     all_reparse = False
     new_indices = False
 
-
     meta_model = ReparseMeta.objects.create(
         fiscal_quarter=fiscal_quarter,
         fiscal_year=fiscal_year,
         all=all_reparse,
         new_indices=new_indices,
-        delete_old_indices=new_indices)
-    
+        delete_old_indices=new_indices,
+    )
+
     # Backup the Postgres DB
     backup_file_name += f"_rpv{meta_model.pk}.pg"
     backup(backup_file_name, log_context)
@@ -76,28 +77,34 @@ def clean_reparse(selected_file_ids):
     # Create and delete Elastic indices if necessary
     handle_elastic(new_indices, log_context)
 
-    file_ids = files.values_list('id', flat=True).distinct()
+    file_ids = files.values_list("id", flat=True).distinct()
     meta_model.total_num_records_initial = count_total_num_records(log_context)
     meta_model.save()
 
     delete_associated_models(meta_model, file_ids, new_indices, log_context)
 
     meta_model.timeout_at = meta_model.created_at + calculate_timeout(
-        num_files,                                                                        
-        meta_model.num_records_deleted)
-    
+        num_files, meta_model.num_records_deleted
+    )
+
     meta_model.save()
-    logger.info(f"Deleted a total of {meta_model.num_records_deleted} records across {num_files} files.")
+    logger.info(
+        f"Deleted a total of {meta_model.num_records_deleted} records across {num_files} files."
+    )
 
     # Delete and re-save datafiles to handle cascading dependencies
-    logger.info(f'Deleting and re-parsing {num_files} files')
+    logger.info(f"Deleting and re-parsing {num_files} files")
     handle_datafiles(files, meta_model, log_context)
 
-    log("Database cleansing complete and all files have been re-scheduling for parsing and validation.",
-            logger_context=log_context,
-            level='info')
-    log(f"Clean and reparse command completed. All files for FY {fiscal_year if fiscal_year else all_fy} and "
-            f"{fiscal_quarter if fiscal_quarter else all_q} have been queued for parsing.",
-            logger_context=log_context,
-            level='info')
-    logger.info('Done. All tasks have been queued to parse the selected datafiles.')
+    log(
+        "Database cleansing complete and all files have been re-scheduling for parsing and validation.",
+        logger_context=log_context,
+        level="info",
+    )
+    log(
+        f"Clean and reparse command completed. All files for FY {fiscal_year if fiscal_year else all_fy} and "
+        f"{fiscal_quarter if fiscal_quarter else all_q} have been queued for parsing.",
+        logger_context=log_context,
+        level="info",
+    )
+    logger.info("Done. All tasks have been queued to parse the selected datafiles.")
