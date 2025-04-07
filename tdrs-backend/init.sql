c5b4ba56bdb89ad2405fe50b5ead86ee559ab9dc
Mo Sohani <msohani@goraft.tech>
2025-04-07 08:43:47 -0400

    user and pass as env var

--- tdrs-backend/init.sql
+++ tdrs-backend/init.sql
@@ -2,5 +2,5 @@ CREATE ROLE readaccess;
 GRANT CONNECT ON DATABASE tdrs_test TO readaccess;
 GRANT USAGE ON SCHEMA public TO readaccess;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readaccess;
-CREATE USER read_grafana_user WITH PASSWORD '12345abc';
-GRANT readaccess TO read_grafana_user;
+CREATE USER $GRAFANA_USER WITH PASSWORD '$GRAFANA_PASSWORD';
+GRANT readaccess TO $GRAFANA_USER;
