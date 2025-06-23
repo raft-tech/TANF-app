e756c77ba57b20073a9f216df227decb094cdae7
Mo Sohani <msohani@goraft.tech>
2025-06-23 10:37:04 -0400

    fixed creating the users and assign views

--- tdrs-backend/scripts/create_readonly_grafana_user.py
+++ tdrs-backend/scripts/create_readonly_grafana_user.py
@@ -6,8 +6,8 @@ from django.conf import settings
 def run(*args):
     """Create a read-only user for the PLG database."""
     # ./manage.py runscript create_readonly_grafana_user --script-args "test" "test2"
-    GRAFANA_PASSWORD = args[0]
-    GRAFANA_USER = args[1]
+    GRAFANA_USER = args[0]
+    GRAFANA_PASSWORD = args[1]
     if not GRAFANA_PASSWORD or not GRAFANA_USER:
         print("Grafana user and password must be provided.")
         return
@@ -17,6 +17,9 @@ def run(*args):
 
     OFA_ADMIN_USER = args[4] if len(args) > 4 else None
     OFA_ADMIN_PASSWORD = args[5] if len(args) > 5 else None
+
+    OFA_DIGIT_TEAM_USER = args[6] if len(args) > 6 else None
+    OFA_DIGIT_TEAM_PASSWORD = args[7] if len(args) > 7 else None
 
     print("Creating Grafana user...")
 
@@ -55,6 +58,14 @@ def run(*args):
                 sql_query = sql_query.replace("$OFA_DB_PASSWORD", OFA_ADMIN_PASSWORD)
             elif "$OFA_DB_PASSWORD" in sql_query and not OFA_ADMIN_PASSWORD:
                 continue
+            if "$DIGIT_READONLY_USER" in sql_query and OFA_DIGIT_TEAM_USER:
+                sql_query = sql_query.replace("$DIGIT_READONLY_USER", OFA_DIGIT_TEAM_USER)
+            elif "$DIGIT_READONLY_USER" in sql_query and not OFA_DIGIT_TEAM_USER:
+                continue
+            if "$DIGIT_READONLY_PASSWORD" in sql_query and OFA_DIGIT_TEAM_PASSWORD:
+                sql_query = sql_query.replace("$DIGIT_READONLY_PASSWORD", OFA_DIGIT_TEAM_PASSWORD)
+            elif "$DIGIT_READONLY_PASSWORD" in sql_query and not OFA_DIGIT_TEAM_PASSWORD:
+                continue
             print(f"--Executing SQL query: {sql_query.strip()}")
             try:
                 with connection.cursor() as cursor:
@@ -66,5 +77,34 @@ def run(*args):
                     print(f"An error occurred: {e}")
                     print("An unexpected error occurred.")
                 continue
+
+        # CREATE DIGIT TEAM USER
+        TANF_SEARCH_INDEX = "tanf_"
+        TANF_SEARCH_TRIBAL = "tribal_tanf_"
+        TANF_SEARCH_SSP = "ssp_"
+        TANF_SEARCH_FRA = "fra_"
+        TANF_DIGIT_TEAM_VIEWS = [
+            TANF_SEARCH_INDEX,
+            TANF_SEARCH_TRIBAL,
+            TANF_SEARCH_SSP,
+            TANF_SEARCH_FRA
+        ]
+
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM information_schema.views")
+            views = cursor.fetchall()
+        
+        for view in views:
+            view_name = view[2]
+            print(f"Checking view: {view_name}")
+            if any([TANF_DIGIT_TEAM_VIEW in view_name for TANF_DIGIT_TEAM_VIEW in TANF_DIGIT_TEAM_VIEWS]):
+                try:
+                    print(f"Granting SELECT on {view_name} to {OFA_DIGIT_TEAM_USER}...")
+                    with connection.cursor() as cursor:
+                        cursor.execute(f"GRANT SELECT ON {view_name} TO {OFA_DIGIT_TEAM_USER};")
+                except Exception as e:
+                    print(f"An error occurred while granting SELECT on {view_name}: {e}")
+            
+
         return
     print("Grafana readonly user created successfully.")
