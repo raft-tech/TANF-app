d7f1edf6f08f5595feb99fd891f2d0404f8d49ef
Mo Sohani <msohani@goraft.tech>
2025-07-14 07:32:43 -0400

    deleted the OFA user

--- tdrs-backend/scripts/create_readonly_grafana_user.py
+++ tdrs-backend/scripts/create_readonly_grafana_user.py
@@ -13,14 +13,11 @@ def run(*args):  # noqa: C901
         print("Grafana user and password must be provided.")
         return
 
-    OFA_USER = args[2] if len(args) > 2 else None
-    OFA_PASSWORD = args[3] if len(args) > 3 else None
+    OFA_ADMIN_USER = args[2] if len(args) > 4 else None
+    OFA_ADMIN_PASSWORD = args[3] if len(args) > 5 else None
 
-    OFA_ADMIN_USER = args[4] if len(args) > 4 else None
-    OFA_ADMIN_PASSWORD = args[5] if len(args) > 5 else None
-
-    OFA_DIGIT_TEAM_USER = args[6] if len(args) > 6 else None
-    OFA_DIGIT_TEAM_PASSWORD = args[7] if len(args) > 7 else None
+    OFA_DIGIT_TEAM_USER = args[4] if len(args) > 6 else None
+    OFA_DIGIT_TEAM_PASSWORD = args[5] if len(args) > 7 else None
 
     print("Creating Grafana user...")
 
@@ -39,15 +36,6 @@ def run(*args):  # noqa: C901
                 sql_query = sql_query.replace("$GRAFANA_PASSWORD", GRAFANA_PASSWORD)
             if "$DB_NAME" in sql_query:
                 sql_query = sql_query.replace("$DB_NAME", DB_NAME)
-
-            if "$OFA_USER" in sql_query and OFA_USER:
-                sql_query = sql_query.replace("$OFA_USER", OFA_USER)
-            elif "$OFA_USER" in sql_query and not OFA_USER:
-                continue
-            if "$OFA_PASSWORD" in sql_query and OFA_PASSWORD:
-                sql_query = sql_query.replace("$OFA_PASSWORD", OFA_PASSWORD)
-            elif "$OFA_PASSWORD" in sql_query and not OFA_PASSWORD:
-                continue
             if "$OFA_DB_USER" in sql_query and OFA_ADMIN_USER:
                 sql_query = sql_query.replace("$OFA_DB_USER", OFA_ADMIN_USER)
             elif "$OFA_DB_USER" in sql_query and not OFA_ADMIN_USER:
