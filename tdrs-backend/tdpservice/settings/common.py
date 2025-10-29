95230a7d099d96faf68b649d6a7b53a123121a39
Mo Sohani <msohani@goraft.tech>
2025-10-29 09:47:10 -0400

    added filter to traces

--- tdrs-backend/tdpservice/settings/common.py
+++ tdrs-backend/tdpservice/settings/common.py
@@ -12,10 +12,12 @@ import django
 from django.core.exceptions import ImproperlyConfigured
 
 import sentry_sdk
+from sentry_sdk.types import SamplingContext
 from celery.schedules import crontab
 from configurations import Configuration
 from corsheaders.defaults import default_headers
 from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -32,6 +34,17 @@ def get_required_env_var_setting(
         )
 
     return env_var
+
+SAMPLER_FILTER_URLS = ["/prometheus/metrics"]
+
+def traces_sampler(sampling_context: SamplingContext) -> float:
+    # Examine provided sampling context along with anything in the
+    # global namespace to compute the sample rate for this transaction
+    if sampling_context.get("wsgi_environ", {}).get("PATH_INFO") in SAMPLER_FILTER_URLS:
+        # Drop this transaction, by setting its sample rate to 0%
+        return 0
+    # Default sample rate for all others (replaces traces_sample_rate)
+    return 0.5
 
 
 def init_sentry(sentry_dsn, environment: str = "ERROR") -> None:
@@ -52,8 +65,9 @@ def init_sentry(sentry_dsn, environment: str = "ERROR") -> None:
                 ],
                 cache_spans=False,
             ),
+            LoggingIntegration(level=logging.ERROR, event_level=logging.ERROR),
         ],
-        traces_sample_rate=1.0,
+        traces_sampler=traces_sampler,
         enable_logs=True,
     )
 
@@ -621,6 +635,7 @@ class Common(Configuration):
     # Sentry config
     SENTRY_DSN = os.getenv("SENTRY_DSN", None)
     SENTRY_ENVIRONMENT = os.getenv("CGAPPNAME_BACKEND", "ERROR")
+
     if SENTRY_DSN:
         init_sentry(sentry_dsn=SENTRY_DSN, environment=SENTRY_ENVIRONMENT)
 
