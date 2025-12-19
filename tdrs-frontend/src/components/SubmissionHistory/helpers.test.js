f20f9bc100028663352fa5e4901dd014d7743cfb
Mo Sohani <msohani@goraft.tech>
2025-12-19 09:42:56 -0500

    fixed filename not showing progtype

---
+++ tdrs-frontend/src/components/SubmissionHistory/helpers.test.js
@@ -0,0 +1,15 @@
+import { formatProgramType } from './helpers'
+
+describe('formatProgramType', () => {
+  it('returns a label for SSP', () => {
+    expect(formatProgramType('SSP')).toEqual('SSP')
+  })
+
+  it('returns a label for Tribal', () => {
+    expect(formatProgramType('TRIBAL')).toEqual('Tribal')
+  })
+
+  it('returns empty string for unknown program type', () => {
+    expect(formatProgramType('UNKNOWN')).toEqual('')
+  })
+})
