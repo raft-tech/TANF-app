2cde34a55495da06e1a76e58cd3c58a259282920
Mo Sohani <msohani@goraft.tech>
2026-08-19 10:30:34 -0400

    5983 moved SVG to folder

--- tdp-admin/src/components/admin-navigation.tsx
+++ tdp-admin/src/components/admin-navigation.tsx
@@ -4,6 +4,27 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useMemo, useState, type MouseEvent } from "react";
 import {
+  AdminNavAppearanceIcon,
+  AdminNavApprovalIcon,
+  AdminNavCollapseIcon,
+  AdminNavDashboardIcon,
+  AdminNavDataFilesIcon,
+  AdminNavEtlIcon,
+  AdminNavFeatureFlagsIcon,
+  AdminNavFeedbackIcon,
+  AdminNavHomeIcon,
+  AdminNavLogEntriesIcon,
+  AdminNavParsersIcon,
+  AdminNavPeriodicTasksIcon,
+  AdminNavReportsIcon,
+  AdminNavSearchIcon,
+  AdminNavSearchIndexesIcon,
+  AdminNavSecurityIcon,
+  AdminNavSttsIcon,
+  AdminNavUsersIcon,
+  type AdminNavIconProps,
+} from "@/components/icons/admin-nav-icons";
+import {
   ADMIN_DASHBOARD_NAV_ITEMS,
   ADMIN_PRIMARY_NAV_ITEMS,
   getDefaultExpandedAdminNavIds,
@@ -20,158 +41,55 @@ function NavIcon({ name }: { name?: AdminNavIconName }) {
     return null;
   }
 
-  return (
-    <svg
-      className="admin-sidenav__icon"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      focusable="false"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="1.8"
-    >
-      {getIconPath(name)}
-    </svg>
-  );
+  return getIcon(name, {
+    className: "admin-sidenav__icon",
+    "aria-hidden": true,
+    focusable: "false",
+  });
 }
 
-function getIconPath(name: AdminNavIconName) {
+function getIcon(name: AdminNavIconName, iconProps: AdminNavIconProps) {
   switch (name) {
     case "appearance":
-      return (
-        <>
-          <path d="M12 3a9 9 0 1 0 0 18h1.25a1.75 1.75 0 0 0 .4-3.45l-.9-.2a1.3 1.3 0 0 1 .3-2.57H15a6 6 0 0 0 0-12h-3Z" />
-          <path d="M7.7 10.4h.1M9.9 7.4h.1M13.2 7.1h.1M16 9.4h.1" />
-        </>
-      );
+      return <AdminNavAppearanceIcon {...iconProps} />;
     case "approval":
-      return (
-        <>
-          <path d="M6 4h9l3 3v13H6z" />
-          <path d="M14 4v4h4M9 14l2 2 4-5" />
-        </>
-      );
+      return <AdminNavApprovalIcon {...iconProps} />;
     case "collapse":
-      return (
-        <>
-          <path d="M4 5h16v14H4zM9 5v14" />
-          <path d="m15 9-3 3 3 3" />
-        </>
-      );
+      return <AdminNavCollapseIcon {...iconProps} />;
     case "dashboard":
-      return (
-        <>
-          <path d="M4 4h7v7H4zM13 4h7v7h-7zM4 13h7v7H4zM13 13h7v7h-7z" />
-        </>
-      );
+      return <AdminNavDashboardIcon {...iconProps} />;
     case "data-files":
-      return (
-        <>
-          <path d="M3.5 6.5h6l1.75 2H20.5v9.5a1.5 1.5 0 0 1-1.5 1.5H5a1.5 1.5 0 0 1-1.5-1.5z" />
-          <path d="M3.5 8.5V6A1.5 1.5 0 0 1 5 4.5h4.25l1.75 2" />
-        </>
-      );
+      return <AdminNavDataFilesIcon {...iconProps} />;
     case "etl":
-      return (
-        <>
-          <path d="M4 7h10M10 3l4 4-4 4" />
-          <path d="M20 17H10M14 13l-4 4 4 4" />
-        </>
-      );
+      return <AdminNavEtlIcon {...iconProps} />;
     case "feature-flags":
-      return (
-        <>
-          <path d="M6 20V4" />
-          <path d="M6 5h11l-1.5 3L17 11H6" />
-        </>
-      );
+      return <AdminNavFeatureFlagsIcon {...iconProps} />;
     case "feedback":
-      return (
-        <>
-          <path d="M5 5h14v10H9l-4 4z" />
-          <path d="M8.5 8.5h7M8.5 11.5h4" />
-        </>
-      );
+      return <AdminNavFeedbackIcon {...iconProps} />;
     case "home":
-      return (
-        <>
-          <path d="m3 10.5 9-7 9 7" />
-          <path d="M5.5 9.5V20h13V9.5" />
-          <path d="M9.5 20v-6h5v6" />
-        </>
-      );
+      return <AdminNavHomeIcon {...iconProps} />;
     case "log-entries":
-      return (
-        <>
-          <path d="M6 4h9l3 3v13H6z" />
-          <path d="M14.5 4v4h3.5M9 12h6M9 15h6M9 18h3" />
-        </>
-      );
+      return <AdminNavLogEntriesIcon {...iconProps} />;
     case "parsers":
-      return (
-        <>
-          <path d="M8 5 4 12l4 7M16 5l4 7-4 7" />
-          <path d="m13.5 6.5-3 11" />
-        </>
-      );
+      return <AdminNavParsersIcon {...iconProps} />;
     case "periodic-tasks":
-      return (
-        <>
-          <path d="M6 5h12a2 2 0 0 1 2 2v11H4V7a2 2 0 0 1 2-2Z" />
-          <path d="M8 3v4M16 3v4M4 10h16M8 15l2 2 4-4" />
-        </>
-      );
+      return <AdminNavPeriodicTasksIcon {...iconProps} />;
     case "reports":
-      return (
-        <>
-          <path d="M5 20V4h14v16z" />
-          <path d="M9 16v-4M12 16V8M15 16v-6" />
-        </>
-      );
+      return <AdminNavReportsIcon {...iconProps} />;
     case "search":
-      return (
-        <>
-          <path d="M11 18a7 7 0 1 1 0-14 7 7 0 0 1 0 14Z" />
-          <path d="m16.5 16.5 3.5 3.5" />
-        </>
-      );
+      return <AdminNavSearchIcon {...iconProps} />;
     case "search-indexes":
-      return (
-        <>
-          <path d="M5 4h8l4 4v5" />
-          <path d="M13 4v5h4M7.5 12h4" />
-          <path d="M13.5 17.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0Z" />
-          <path d="m19.5 20.5 1.5 1.5" />
-        </>
-      );
+      return <AdminNavSearchIndexesIcon {...iconProps} />;
     case "security":
-      return (
-        <>
-          <path d="M12 3 5 6v5c0 4.25 2.75 7.5 7 10 4.25-2.5 7-5.75 7-10V6z" />
-          <path d="m9 12 2 2 4-5" />
-        </>
-      );
+      return <AdminNavSecurityIcon {...iconProps} />;
     case "stts":
-      return (
-        <>
-          <circle cx="6.5" cy="7" r="2.5" />
-          <circle cx="17.5" cy="7" r="2.5" />
-          <circle cx="12" cy="17" r="2.5" />
-          <path d="m8 9 2.75 5M16 9l-2.75 5M9 17h-1" />
-        </>
-      );
+      return <AdminNavSttsIcon {...iconProps} />;
     case "users":
-      return (
-        <>
-          <path d="M8.5 11a3.25 3.25 0 1 0 0-6.5 3.25 3.25 0 0 0 0 6.5Z" />
-          <path d="M3.5 20a5 5 0 0 1 10 0" />
-          <path d="M16 10.5a2.75 2.75 0 1 0 0-5.5" />
-          <path d="M16 14.5a4 4 0 0 1 4 4" />
-        </>
-      );
+      return <AdminNavUsersIcon {...iconProps} />;
   }
+
+  const exhaustiveCheck: never = name;
+  return exhaustiveCheck;
 }
 
 function navItemMatchesSearch(item: AdminNavItem, query: string) {
