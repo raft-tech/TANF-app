c83f3ae605bd934f6613e526d130364d7e985bdf
Mo Sohani <msohani@goraft.tech>
2026-08-26 18:32:52 -0400

    Show admin navigation for Django administrators

--- tdp-admin/src/lib/admin-auth.ts
+++ tdp-admin/src/lib/admin-auth.ts
@@ -15,6 +15,8 @@ export type AdminSession = {
     email?: string;
     first_name?: string;
     last_name?: string;
+    is_staff?: boolean;
+    is_superuser?: boolean;
     roles?: (
       | string
       | {
